640a76b82d958e063e69c2eb90423ed19b8bd2ef
Mateus Durães <72138403+matt-duraes@users.noreply.github.com>
2022-05-25 09:54:14 -0300

    Update tabela.sql

--- tabela.sql
+++ tabela.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE marktclub3;
+USE marktclub3;
 
 CREATE TABLE users (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -11,7 +12,7 @@ CREATE TABLE users (
   status INTEGER UNSIGNED NULL,
   PRIMARY KEY(id)
 );
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `cpf`, `token`, `permissao`, `status`) VALUES (NULL, 'admin', 'marktclub@gmail.com', '$2y$10$ZdabkJ2nmCLChbs4SbNlkOoJlAoUa0MynaGL', '07385656165', '', 'login,usuario_add,usuario_editar,usuario_deletar', '1');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `cpf`, `token`, `permissao`, `status`) VALUES (NULL, 'admin', 'marktclub@gmail.com', '$2y$10$OyY0BRwPCEBDxWVVkfCyAeUU/09SNXN40zOeFdfMTVYeEpwVzvEjG', '07385656165', '', 'login,usuario_add,usuario_editar,usuario_deletar', '1');
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `cpf`, `token`, `permissao`, `status`) VALUES (NULL, 'user', 'user@gmail.com', '$2y$10$nTqCaYNbdfMyd/DSCoZALeL6aqQGC6LkiJ20fAkr.WBVXY0ISkpZu', '08524968425', '', 'login,usuario_add,usuario_editar', '1');
 
