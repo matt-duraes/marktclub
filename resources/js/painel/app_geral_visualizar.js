ca6defa0a72752b165ca2d759db66a47e23d3cd1
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2022-08-24 18:16:32 -0300

    Tarefa: Botao do visualizar passando por baixo do historico

--- resources/js/painel/app_geral_visualizar.js
+++ resources/js/painel/app_geral_visualizar.js
@@ -7,8 +7,6 @@ window.addEventListener('load', () => {
     botaoLista.forEach(botao => {
         const status = botao.getAttribute('data-status') || '';
         const mensagem = botao.getAttribute('data-mensagem') || '';
-        console.log(status);
-        console.log(mensagem);
         if (status == '' || mensagem == '') {
             return;
         }
@@ -19,6 +17,31 @@ window.addEventListener('load', () => {
             }
         });
     });
+
+    const blocoVisualizar = document.querySelector('#bloco_app_visualizar');
+    const blocoMain = document.querySelector('#main_template');
+    const blocoBotaoLista = document.querySelector('.bloco_status_lista');
+    if (blocoBotaoLista) {
+        blocoMain.addEventListener('scroll', e => {
+            validarScrollPositionBotao();
+        });
+    }
+    const validarScrollPositionBotao = () => {
+        const posicao = blocoVisualizar.getBoundingClientRect();
+        const scrollTop = blocoMain.scrollTop;
+        const alturaTela = window.innerHeight;
+
+        if (posicao.height - alturaTela <= 0) {
+            return;
+        }
+
+        const botaoScrollAltura = posicao.height - alturaTela + 60;
+        if (scrollTop > botaoScrollAltura && !blocoBotaoLista.classList.contains('relative')) {
+            blocoBotaoLista.classList.add('relative');
+        } else if (scrollTop <= botaoScrollAltura && blocoBotaoLista.classList.contains('relative')) {
+            blocoBotaoLista.classList.remove('relative');
+        }
+    };
 
     const idVisualizar = document.querySelector('#input_visualizar_id').value;
     const appVisualizar = document.querySelector('#input_visualizar_app').value;
