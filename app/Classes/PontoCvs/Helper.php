d242e69cafae098b347fc21859844b14efa33f7e
brian-luca <Tehion@hotmail.com>
2022-10-04 11:24:28 -0300

    Bugfix voltando para metodo de criptografia antigo

--- app/Classes/PontoCvs/Helper.php
+++ app/Classes/PontoCvs/Helper.php
@@ -5,9 +5,7 @@ namespace App\Classes\PontoCvs;
 final class Helper
 {
     const CRIPTOGRAFAR = [
-        'usuario' => [
             'matricula', 'nome', 'cpf', 'email', 'telefone', 'credito', 'debito', 'saldo'
-            ]
     ];
     const PONTO_MINIMO = 1;
 }
--- database/usuario_novo/dado.php
+++ database/usuario_novo/dado.php
@@ -29,10 +29,13 @@ return [
         'status' => 1
     ],
     [
+        'matricula' => 1354826,
         'cod' => uuid(),
         'empresa' => 198,
         'tipo' => 1,
         'nome' => nomeAleatorio(),
+        'email_trabalho' => emailAleatorio(),
+        'telefone_fixo' => telefoneAleatorio(),
         'documento' => 67783406815,
         'salt' => password('Teste@1324'),
         'status' => 1
