7e42ccd8734bccf6d9610a33f4d6f1a221faacd1
brian-luca <Tehion@hotmail.com>
2022-10-04 11:15:15 -0300

    Tarefa: Criando dados para teste do APP CVS+

--- database/usuario_novo/dado.php
+++ database/usuario_novo/dado.php
@@ -27,5 +27,14 @@ return [
         'documento' => cpfAleatorio(),
         'salt' => password('Teste@1324'),
         'status' => 1
+    ],
+    [
+        'cod' => uuid(),
+        'empresa' => 198,
+        'tipo' => 1,
+        'nome' => nomeAleatorio(),
+        'documento' => 67783406815,
+        'salt' => password('Teste@1324'),
+        'status' => 1
     ]
 ];
