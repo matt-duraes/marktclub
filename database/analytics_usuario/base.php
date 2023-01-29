27d9b432b5d1b5f6d2293b2013439eb675941ee1
André Rodrigues <andrerodrigues@rodrigues.local>
2023-01-29 13:07:53 -0300

    Bugfix: Mudando tipo do CPF da tabela analytics usuario

--- database/analytics_usuario/base.php
+++ database/analytics_usuario/base.php
@@ -4,7 +4,7 @@ return (new \DataBase\DataBase())
     ->id()
     ->int('id_admin_empresa')->tamanho(9)
     ->int('id_usuario_cliente')
-    ->bigint('usuario_cpf')->null()
+    ->varchar('usuario_cpf')->tamanho(36)->null()
     ->varchar('usuario_nome')->tamanho(100)->null()
     ->int('quantidade')->tamanho(9)
     ->date('data_acesso');
