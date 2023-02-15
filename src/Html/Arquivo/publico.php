a1725dd66ffe8f2f0661843b8b85b753316d3c47
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-02-15 12:45:18 -0300

    Tarefa: Corrigindo erro dos arquivos

--- src/Html/Arquivo/publico.php
+++ src/Html/Arquivo/publico.php
@@ -6,16 +6,16 @@ $explode = explode('?', $requestUri);
 $nome = arquivoPublicoNome($explode[0]);
 
 if (empty($nome)) {
-    mensagemStatus(404, 'O arquivo está sem nome.');
+    mensagemStatus(404, localhost: 'O arquivo está sem nome.');
 }
 
 $path = DIRETORIO_PUBLICO . '/' . preg_replace('/^\//', '', $nome);
 $download = array_key_exists('download', $_GET) && $_GET['download'] == 1;
 
 if (!file_exists($path)) {
-    mensagemStatus(404, 'O arquivo não existe.');
+    mensagemStatus(404, localhost: 'O arquivo não existe.');
 } else if (!is_file($path)) {
-    mensagemStatus(404, 'O arquivo não é um arquivo comum.');
+    mensagemStatus(404, localhost: 'O arquivo não é um arquivo comum.');
 } else if (!$download) {
     $Response = new Response(arquivo: $path);
     $Response->render();
