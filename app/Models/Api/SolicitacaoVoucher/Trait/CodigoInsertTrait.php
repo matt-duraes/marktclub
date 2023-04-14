6c6574ef10d8b7179fa83603717a502a3b97ea5b
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-04-14 14:54:15 -0300

    Tarefa: Colocando data fixa do voucher de codigo

--- app/Models/Api/SolicitacaoVoucher/Trait/CodigoInsertTrait.php
+++ app/Models/Api/SolicitacaoVoucher/Trait/CodigoInsertTrait.php
@@ -2,7 +2,6 @@
 
 namespace App\Models\Api\SolicitacaoVoucher\Trait;
 
-use Modules\Data;
 use Modules\DataHora;
 use App\Classes\SolicitacaoCodigo\Status;
 
@@ -21,17 +20,8 @@ trait CodigoInsertTrait
         $this->id_admin_empresa = $this->Usuario->id_admin_empresa;
         $this->id_usuario_cliente = $this->Usuario->get('id');
         $this->id_parceiro_loja = $this->Parceiro->get('id');
-        $this->data_vencimento = new Data($this->pegarVencimentoVoucher());
         $this->data_emissao = new DataHora(agora());
         $this->status = new Status(Status::SOLICITADO);
-    }
-
-    private function pegarVencimentoVoucher()
-    {
-        if ($this->Parceiro->prazo_voucher_fixo->valido()) {
-            return $this->Parceiro->prazo_voucher_fixo->date();
-        }
-        return dataAdicionar(hoje(), $this->Parceiro->prazo_voucher, 'dias');
     }
 
     private function verificarSeJaExisteVoucher(): bool
@@ -59,8 +49,10 @@ trait CodigoInsertTrait
         $id = $this
             ->campo(['id', 'codigo'])
             ->where([
-                ['status', 1]
-            ])->primeiro(campo: 'id');
+                ['status', 1],
+                ['id_parceiro_loja', $this->Parceiro->get('id')],
+            ])
+            ->primeiro(campo: 'id');
 
         if (empty($id)) {
             mensagemErro('Sem voucher!', 'Os vouchers esgotaram no momento, estamos providenciando mais vouchers.');
