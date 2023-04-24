ae54c2b35c199cb9e2d8d117a1ec1a04d2e5fce3
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-04-24 17:12:28 -0300

    fix: correcao de namespace errado nas controllers do site

--- app/Controllers/Site/LojaController.php
+++ app/Controllers/Site/LojaController.php
@@ -2,30 +2,25 @@
 
 namespace App\Controllers\Site;
 
+use App\Models\Site\BannerModel;
+use App\Models\Site\Loja\BuscaModel;
+use App\Models\Site\Loja\ListarModel;
+use App\Models\Site\Loja\RelacionadoModel;
+use Controller\Controller;
+use Erro\Excecao;
 use Http\Request;
 use Http\Response;
-use Controller\Controller;
-use App\Models\Site\BannerModel;
-use App\Models\Site\Loja\MapaModel;
-use App\Models\Site\Loja\BuscaModel;
-use App\models\Site\Loja\ListarModel;
-use App\models\Site\Loja\RelacionadoModel;
 
 final class LojaController extends Controller
 {
-    public function index(Request $request, ?BuscaModel $Busca = null)
-    {
-        return view('loja.index', [
-            'menu' => 'loja',
-            'banner' => true,
-            'Busca' => $Busca instanceof BuscaModel ? $Busca : new BuscaModel($request),
-            'lista' => (new ListarModel())->listarDados(),
-            'parceiroTipo' => 'loja',
-            'banner' => (new BannerModel())->loja(),
-        ]);
-    }
-
-    public function busca(Request $request, ?string $pesquisa = null)
+    /**
+     * @param  Request      $request
+     * @param  string|null  $pesquisa
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function busca(Request $request, string $pesquisa = null): Response
     {
         $Busca = new BuscaModel($request, $pesquisa);
         if ($pesquisa) {
@@ -34,48 +29,98 @@ final class LojaController extends Controller
         return new Response(url: $Busca->url());
     }
 
-    public function detalhe(Request $request, $url = null, ?BuscaModel $Busca = null)
+    /**
+     * @param  Request          $request
+     * @param  BuscaModel|null  $Busca
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function index(Request $request, BuscaModel $Busca = null): Response
+    {
+        return view('loja.index', [
+            'menu'         => 'loja',
+            'banner'       => true,
+            'Busca'        => $Busca instanceof BuscaModel ? $Busca : new BuscaModel($request),
+            'lista'        => (new ListarModel())->listarDados(),
+            'parceiroTipo' => 'loja',
+            'banner'       => (new BannerModel())->loja()
+        ]);
+    }
+
+    /**
+     * @param  Request          $request
+     * @param                   $url
+     * @param  BuscaModel|null  $Busca
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function detalhe(Request $request, $url = null, BuscaModel $Busca = null): Response
     {
         return view('loja.detalhe', [
-            'menu' => 'loja',
-            'url' => $url,
-            'Busca' => $Busca instanceof BuscaModel ? $Busca : new BuscaModel($request),
-            'lista' => (new RelacionadoModel())->listarDados(),
+            'menu'         => 'loja',
+            'url'          => $url,
+            'Busca'        => $Busca instanceof BuscaModel ? $Busca : new BuscaModel($request),
+            'lista'        => (new RelacionadoModel())->listarDados(),
             'parceiroTipo' => 'loja'
         ]);
     }
-    public function confirmar(string $url)
+
+    /**
+     * @return Response
+     * @throws Excecao
+     */
+    public function confirmar(): Response
     {
         return view('loja.confirmar');
     }
 
-    public function proxima(Request $request, ?MapaModel $Busca = null)
+    /**
+     * @return Response
+     * @throws Excecao
+     */
+    public function proxima(): Response
     {
         return view('loja.proxima', [
             'menu' => 'loja-proxima'
         ]);
     }
 
-    public function abrirModal(Request $request)
+    /**
+     * @return Response
+     * @throws Excecao
+     */
+    public function abrirModal(): Response
     {
         return view('loja.geral.modal');
     }
 
-    public function abrirModalIndicacao()
+    /**
+     * @return Response
+     * @throws Excecao
+     */
+    public function abrirModalIndicacao(): Response
     {
         return view('loja.geral.modalIndicacao');
     }
-    public function abrirMapaModal()
+
+    /**
+     * @return Response
+     * @throws Excecao
+     */
+    public function abrirMapaModal(): Response
     {
         return view('loja.proxima.modal', [
             'menu' => 'loja-proxima'
         ]);
     }
 
-    public function postBuscaMapa(Request $request)
+    /**
+     * @return Response
+     */
+    public function postBuscaMapa(): Response
     {
-        $Busca = new MapaModel($request);
-
         return new Response(status: 201);
     }
 }
