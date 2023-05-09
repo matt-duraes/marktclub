4c56192f72056baff6a7a7c5d86b1ef62ce42464
matt-duraes <mateusduraessilva@gmail.com>
2023-05-09 12:13:44 -0300

    Bugfix: ajustando espacamentos

--- app/Controllers/Site/AlfaController.php
+++ app/Controllers/Site/AlfaController.php
@@ -13,44 +13,56 @@ final class AlfaController extends Controller
         return view(
             'alfa.credito',
             [
-            'menu' => 'credito',
-            'banner' => (new BannerModel())->alfa()
+                'menu' => 'credito',
+                'banner' => (new BannerModel())->alfa()
             ]
         );
     }
 
     public function portabilidade()
     {
-        return view('alfa.portabilidade', [
-            'menu' => 'credito',
-            'banner' => (new BannerModel())->alfa(),
-            'parcela' => (new CreditoModel())->listarParcelas()
-        ]);
+        return view(
+            'alfa.portabilidade',
+            [
+                'menu' => 'credito',
+                'banner' => (new BannerModel())->alfa(),
+                'parcela' => (new CreditoModel())->listarParcelas()
+            ]
+        );
     }
 
     public function consignado()
     {
-        return view('alfa.consignado', [
-            'menu' => 'credito',
-            'banner' => (new BannerModel())->alfa(),
-            'parcela' => (new CreditoModel())->listarParcelas()
-        ]);
+        return view(
+            'alfa.consignado',
+            [
+                'menu' => 'credito',
+                'banner' => (new BannerModel())->alfa(),
+                'parcela' => (new CreditoModel())->listarParcelas()
+            ]
+        );
     }
 
     public function veiculo()
     {
-        return view('alfa.veiculo', [
-            'menu' => 'credito',
-            'parceiro' => [1, 2, 3]
-        ]);
+        return view(
+            'alfa.veiculo',
+            [
+                'menu' => 'credito',
+                'parceiro' => [1, 2, 3]
+            ]
+        );
     }
 
     public function corretoraAlfa()
     {
-        return view('alfa.corretora', [
-            'banner' => (new BannerModel())->corretora(),
-            'menu' => 'corretora_alfa'
-        ]);
+        return view(
+            'alfa.corretora',
+            [
+                'banner' => (new BannerModel())->corretora(),
+                'menu' => 'corretora_alfa'
+            ]
+        );
     }
 
     public function consultoriaAlfa()
