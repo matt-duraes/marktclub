fdd7107e6cf98657f9cd09e7d5627ed178021eff
kapcruz <karina.pereira@feedz.com.br>
2023-05-21 03:09:56 -0300

    refactor: utilizando padroes do sistema para o cinema

--- app/Controllers/Site/CinemaController.php
+++ app/Controllers/Site/CinemaController.php
@@ -3,16 +3,23 @@
 namespace App\Controllers\Site;
 
 use App\Models\Site\BannerModel;
+use App\Models\Site\Loja\ListarModel;
 
 use Controller\Controller;
 
 final class CinemaController extends Controller
 {
+    /**
+     * @return Response
+     * @throws Excecao
+     */
     public function index(?string $pesquisa = null)
     {
         return view('cinema.index', [
             'menu'   => 'cinema',
-            'banner' => (new BannerModel())->cinema()
+            'banner' => (new BannerModel())->cinema(),
+            'lista' => (new ListarModel())->listarDados(),
+            'parceiroTipo' => 'cinema',
         ]);
     }
 
