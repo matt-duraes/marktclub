b62104b3f2900fcd58b8c88cc1b81b2fc5d9b21b
kapcruz <karina.pereira@feedz.com.br>
2023-05-27 19:27:42 -0300

    fix: criando funcao para chamar accordion apenas quando tem demanda

--- resources/js/site/accordion.js
+++ resources/js/site/accordion.js
@@ -1,27 +1,32 @@
 function Accordion() {
-    this.items = document.querySelectorAll('.accordion_item');
-    this.items.forEach(function (item) {
+    var items = document.querySelectorAll('.accordion_item');
+
+    items.forEach(function (item) {
         var titulo = item.querySelector('.accordion_titulo');
-        titulo.addEventListener('click', this.toggleItem.bind(this, item));
-    }, this);
-}
-
-Accordion.prototype.toggleItem = function (item) {
-    var conteudo = item.querySelector('.accordion_conteudo');
-    var seAberto = conteudo.classList.contains('abrir');
-
-    this.fechaTodosItems();
-
-    if (!seAberto) {
-        conteudo.classList.add('abrir');
-        item.classList.toggle('abrir');
-    }
-};
-
-Accordion.prototype.fechaTodosItems = function () {
-    this.items.forEach(function (item) {
-        item.querySelector('.accordion_conteudo').classList.remove('abrir');
-        item.classList.remove('abrir');
+        titulo.addEventListener('click', function () {
+            toggleItem(item);
+        });
     });
-};
-var accordion = new Accordion();
+
+    function toggleItem(item) {
+        var conteudo = item.querySelector('.accordion_conteudo');
+        var seAberto = conteudo.classList.contains('abrir');
+
+        fechaTodosItems();
+
+        if (!seAberto) {
+            conteudo.classList.add('abrir');
+            item.classList.toggle('abrir');
+        }
+    }
+
+    function fechaTodosItems() {
+        items.forEach(function (item) {
+            item.querySelector('.accordion_conteudo').classList.remove('abrir');
+            item.classList.remove('abrir');
+        });
+    }
+}
+function criarAccordion() {
+    var accordion = new Accordion();
+}
--- views/pages/site/campanha/tematica/js/all.js
+++ views/pages/site/campanha/tematica/js/all.js
@@ -1,2 +1,6 @@
 // @template "site"
 // @resource "site/accordion"
+
+window.addEventListener('load', () => {
+    criarAccordion();
+});
