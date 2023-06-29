f4b91270770a8a1202ec8d2f4215681ace7d4634
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-06-29 10:47:19 -0300

    improvement(api): adicionando dados ficticios para teste

--- database/publicidade/dado.php
+++ database/publicidade/dado.php
@@ -1,4 +1,54 @@
 <?php
 
+use App\Classes\Publicidade\Tipo;
+
 return [
+    [
+        'uuid'     => uuid(),
+        'empresa'  => numeroAleatorio(1, 10),
+        'titulo'   => 'lorem ipson asda diow milop - ' . numeroAleatorio(1, 10),
+        'imagem'   => 'https://via.placeholder.cpm/' . valorAleatorio([1000, 2000, 3000, 4000, 5000]),
+        'link'     => 'https://via.placeholder.cpm/1920x1080?text=MARKTCLUB',
+        'target'   => '_self',
+        'parceiro' => '',
+        'ordem'    => 1,
+        'tipo'     => valorAleatorio((new Tipo())->listarNumero()),
+        'status'   => 4
+    ],
+    [
+        'uuid'     => uuid(),
+        'empresa'  => numeroAleatorio(1, 10),
+        'titulo'   => 'lorem ipson asda diow milop - ' . numeroAleatorio(1, 10),
+        'imagem'   => 'https://via.placeholder.cpm/' . valorAleatorio([1000, 2000, 3000, 4000, 5000]),
+        'link'     => 'https://via.placeholder.cpm/1920x1080?text=MARKTCLUB',
+        'target'   => '_blank',
+        'parceiro' => '',
+        'ordem'    => 1,
+        'tipo'     => valorAleatorio((new Tipo())->listarNumero()),
+        'status'   => 4
+    ],
+    [
+        'uuid'     => uuid(),
+        'empresa'  => numeroAleatorio(1, 10),
+        'titulo'   => 'lorem ipson asda diow milop - ' . numeroAleatorio(1, 10),
+        'imagem'   => 'https://via.placeholder.cpm/' . valorAleatorio([1000, 2000, 3000, 4000, 5000]),
+        'link'     => 'https://via.placeholder.cpm/1920x1080?text=MARKTCLUB',
+        'target'   => '_self',
+        'parceiro' => '',
+        'ordem'    => 1,
+        'tipo'     => valorAleatorio((new Tipo())->listarNumero()),
+        'status'   => 4
+    ],
+    [
+        'uuid'     => uuid(),
+        'empresa'  => numeroAleatorio(1, 10),
+        'titulo'   => 'lorem ipson asda diow milop - ' . numeroAleatorio(1, 10),
+        'imagem'   => 'https://via.placeholder.cpm/' . valorAleatorio([1000, 2000, 3000, 4000, 5000]),
+        'link'     => 'https://via.placeholder.cpm/1920x1080?text=MARKTCLUB',
+        'target'   => '_blank',
+        'parceiro' => '',
+        'ordem'    => 1,
+        'tipo'     => valorAleatorio((new Tipo())->listarNumero()),
+        'status'   => 4
+    ]
 ];
