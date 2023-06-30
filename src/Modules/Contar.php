2ae73607b7f5482d5fcca97eb832ca65b5addcf1
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-06-30 09:21:55 -0300

    Feat: Colocando modulo de contar

---
+++ src/Modules/Contar.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Modules;
+
+use Modules\Trait\ValidarTrait;
+
+final class Botao implements ModuleInterface
+{
+    use ValidarTrait;
+
+    private int $numero = 0;
+    public function __toString()
+    {
+        return $this->valor();
+    }
+
+    // doc
+    /**
+     * Gera um modulo de botão
+     *
+     * @param  null|int|string   $valor  Valor podendo ser string sendo sim ou nao ou um int com
+     *                                   valor 1 para sim ou vazio para nao
+     */
+    public function __construct(
+        private null|int|string $valor,
+    ) {
+        if (!is_numeric($valor) && empty($valor)) {
+            $this->valido = false;
+            $this->vazio = true;
+            return;
+        } elseif (!is_numeric($valor)) {
+            $this->valido = false;
+            $this->vazio = false;
+            return;
+        } elseif (is_numeric($valor)) {
+            return;
+        }
+
+        $this->numero = $valor;
+    }
+
+    // doc
+    /**
+     * Adicionar um número ao valor atual
+     *
+     * @param  int  $numero  Número a ser adicionado
+    */
+    public function adicionar(int $numero): void
+    {
+        $this->numero += $numero;
+    }
+
+    // doc
+    /**
+     * Remove um número ao valor atual
+     *
+     * @param  int  $numero  Número a ser removido
+     */
+    public function remover(int $numero): void
+    {
+        $this->numero -= $numero;
+    }
+
+    // doc
+    /**
+     * Pega o numero atual
+     *
+     * @return int
+     */
+    public function numero(): int
+    {
+        return $this->numero;
+    }
+
+    // doc
+    /**
+     * Pegar o numero atual
+     *
+     * @return string Retorna sim ou nao
+     */
+    public function valor(): int
+    {
+        return $this->numero;
+    }
+}
