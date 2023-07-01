40201f24b7084a26dcf0458bb9825043c7832e9a
kapcruz <karina.pereira@feedz.com.br>
2023-07-01 17:58:30 -0300

    refactor: removendo dados para mensagem do automovel

--- database/auth_app/dado.php
+++ database/auth_app/dado.php
@@ -190,9 +190,7 @@ EwIDAQAB
 
             "contato:salvar", "contato:listar", "contato:buscar",
 
-            "mensagem_indicacao_parceiro:salvar", "mensagem_indicacao_parceiro:listar", "mensagem_indicacao_parceiro:buscar",
-
-            "mensagem_indicacao_automovel:salvar"
+            "mensagem_indicacao_parceiro:salvar", "mensagem_indicacao_parceiro:listar", "mensagem_indicacao_parceiro:buscar"
         ],
         'campo_permitido' => [
             "usuario:salvar" => ["nome", "cpf", "email_trabalho", "senha", "telefone_celular"],
--- routes/ApiRoute.php
+++ routes/ApiRoute.php
@@ -1455,17 +1455,3 @@ Route
             ::middleware(TokenMiddleware::class, 'scope', ['mensagem_indicacao_parceiro:buscar'])
             ::get('/parceiro-indicacao/{id}');
     });
-
-Route
-    ::nome('automovel')
-    ::controller(App\Controllers\Api\IndicacaoAutomovelController::class)
-    ::middleware(TokenMiddleware::class, 'token')
-    ::grupo(function () {
-        Route
-            ::nome('salvar')
-            ::middleware(TokenMiddleware::class, 'scope', ['mensagem_indicacao_automovel:salvar'])
-            ::request([
-                'produto', 'modelo', 'versao', 'cor', 'cidade', 'mensagem'
-            ])
-            ::post('/automovel/indicacao');
-    });
