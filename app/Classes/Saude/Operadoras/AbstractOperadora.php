53351af741a041ee29e70c5fb13a8c660c97f351
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-07-13 11:24:56 -0300

    fix(api-saude): corrigido bug que nao encontrava acomodacoes

--- app/Classes/Saude/Operadoras/AbstractOperadora.php
+++ app/Classes/Saude/Operadoras/AbstractOperadora.php
@@ -51,10 +51,11 @@ abstract class AbstractOperadora implements OperadoraInterface
     ])]
     public function pegarDados(): array
     {
+        $acomodacoes = ($this->plano !== null) ? $this->acomodacoes[$this->plano->indice()] : $this->acomodacoes;
         return [
             'data_nascimento' => $this->dataNascimento,
             'acomodacao'      => $this->acomodacao,
-            'acomodacoes'     => $this->acomodacoes,
+            'acomodacoes'     => $acomodacoes,
             'plano'           => $this->plano,
             'regiao'          => $this->regiao,
         ];
