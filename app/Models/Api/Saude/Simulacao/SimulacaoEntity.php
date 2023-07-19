104d0c6c6d6850b7bbda4f4eebd08270d923e907
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-07-19 10:19:07 -0300

    fix(api-saude): correcao na simulacao de saude

--- app/Models/Api/Saude/Simulacao/SimulacaoEntity.php
+++ app/Models/Api/Saude/Simulacao/SimulacaoEntity.php
@@ -28,9 +28,9 @@ class SimulacaoEntity extends Entity
     public Operadora $operadora;
     public int $acomodacao;
     public Regiao $regiao;
-    public Dinheiro $valorTitular;
-    public string $valorDependentes;
-    public Dinheiro $valorTotal;
+    public Dinheiro $valor_titular;
+    public string $valor_dependentes;
+    public Dinheiro $valor_total;
     public Plano $plano;
     public Status $status;
     protected ?int $idEmpresa;
@@ -92,36 +92,36 @@ class SimulacaoEntity extends Entity
         };
 
         $dependentes = [];
-        if (is_array($this->request->getPost('dependentes')) && !empty($this->request->getPost('dependentes'))) {
+        if (!empty($this->request->getPost('dependentes'))) {
             $dependentes = explode(',', $this->request->getPost('dependentes'));
             $this->quantidade_dependentes = count($dependentes);
 
             $contador = 1;
-            for ($i = 0; $i <= $this->quantidade_dependentes; $i++) {
+            for ($i = 0; $i < $this->quantidade_dependentes; $i++) {
                 (new ValidarHelper())
                     ->valor($dependentes[$i], 'Dependente ' . $contador)
                     ->obrigatorio()
-                    ->date();
+                    ->data();
                 $contador++;
             }
         }
 
-        $contador = 1;
-        $valorTitular = $planoSaude->valor;
-        $valorDependentes = [];
-        $valorTotal = $valorTitular;
+        $valor_titular = $planoSaude->valor;
+        $valor_dependentes = [];
+        $valor_total = $valor_titular;
         if ($this->quantidade_dependentes > 0) {
-            for ($i = 0; $i <= $this->quantidade_dependentes; $i++) {
+            $contador = 1;
+            for ($i = 0; $i < $this->quantidade_dependentes; $i++) {
                 $valor = $planoSaude->simularValor(new Data($dependentes[$i]));
-                $valorDependentes['dependente-' . $contador] = $valor;
-                $valorTotal = $valorTotal + $valor;
+                $valor_dependentes['dependente-' . $contador] = $valor;
+                $valor_total = $valor_total + $valor;
                 $contador++;
             }
         }
 
         $this->acomodacao = $planoSaude->codigoAcomodacao;
-        $this->valorTitular = new Dinheiro((string)$valorTitular);
-        $this->valorDependentes = jsonEncode($valorDependentes);
-        $this->valorTotal = new Dinheiro((string)$valorTotal);
+        $this->valor_titular = new Dinheiro((string)$valor_titular);
+        $this->valor_dependentes = jsonEncode($valor_dependentes);
+        $this->valor_total = new Dinheiro((string)$valor_total);
     }
 }
