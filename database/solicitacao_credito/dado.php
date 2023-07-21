5072f1e07efa40b8f816e2638fe5ea974a53bee6
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-07-21 15:18:53 -0300

    improvement(database): melhorias na criacao da tabela de credito

--- database/solicitacao_credito/dado.php
+++ database/solicitacao_credito/dado.php
@@ -1,43 +1,21 @@
 <?php
 
+use App\Classes\SolicitacaoCredito\Operadora;
+use App\Classes\SolicitacaoCredito\Status;
+use App\Classes\SolicitacaoCredito\Tipo;
+
 return [
     [
-        'uuid'           => uuid(),
-        'codigo'         => uuid(),
-        'usuario'        => 1,
-        'empresa'        => 1,
-        'operadora'      => 1,
-        'tipo'           => 1,
-        'valor'          => numeroAleatorio(1, 100000),
-        'parcelas'       => numeroAleatorio(1, 96),
-        'valor_parcelas' => numeroAleatorio(1, 100000),
-        'observacao'     => '',
-        'status'         => 1
-    ],
-    [
-        'uuid'           => uuid(),
-        'codigo'         => uuid(),
-        'usuario'        => 1,
-        'empresa'        => 1,
-        'operadora'      => 1,
-        'tipo'           => 3,
-        'valor'          => numeroAleatorio(1, 100000),
-        'parcelas'       => numeroAleatorio(1, 96),
-        'valor_parcelas' => numeroAleatorio(1, 100000),
-        'observacao'     => '',
-        'status'         => 2
-    ],
-    [
-        'uuid'           => uuid(),
-        'codigo'         => uuid(),
-        'usuario'        => 1,
-        'empresa'        => 1,
-        'operadora'      => 1,
-        'tipo'           => 2,
-        'valor'          => numeroAleatorio(1, 100000),
-        'parcelas'       => numeroAleatorio(1, 96),
-        'valor_parcelas' => numeroAleatorio(1, 100000),
-        'observacao'     => '',
-        'status'         => 3
+        'uuid'             => 'c9c6d7cd-27d3-471c-b7e0-73fc7026368d',
+        'codigo'           => uuid(),
+        'id_admin_empresa' => 1,
+        'usuario'          => 1,
+        'operadora'        => (new Operadora(Operadora::SICOOB))->numero(),
+        'tipo'             => (new Tipo(Tipo::CONSIGNADO))->numero(),
+        'valor'            => number_format(numeroAleatorio(1, 100000), 2, '.', ''),
+        'parcelas'         => numeroAleatorio(1, 96),
+        'valor_parcelas'   => number_format(numeroAleatorio(1, 100000), 2, '.', ''),
+        'observacao'       => '',
+        'status'           => (new Status(Status::CRIADA))->numero()
     ]
 ];
