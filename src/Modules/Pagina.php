29aac7b2056609c3705d8a2f9df38d2c92c08f72
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-07-25 15:06:25 -0300

    Feat: Criando cashback

---
+++ src/Modules/Pagina.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Modules;
+
+use Modules\Trait\ValidarTrait;
+
+final class Pagina implements ModuleInterface
+{
+    use ValidarTrait;
+
+    private string $valor = '';
+
+    public function __toString()
+    {
+        return $this->numero();
+    }
+
+    public function padrao()
+    {
+        return $this->vazio() ? 1 : $this->numero();
+    }
+
+    /**
+     * Pega o valor padrão independente do tipo de modulo
+     */
+    public function valor()
+    {
+        return $this->numero();
+    }
+
+    // doc
+    /**
+     * Gera um modulo de gênero
+     *
+     * @param null|int|string $genero Valor do Genero podendo ser string ou int quando vier do banco
+     */
+    public function __construct(
+        private null|int|string $numero = null,
+    ) {
+        if (empty($numero)) {
+            $this->vazio = true;
+            $this->valido = false;
+            $this->numero = '';
+            return;
+        } elseif (!preg_match('/^[1-9]{1}[0-9]{0,}$/', $numero)) {
+            $this->valido = false;
+            $this->numero = '';
+            return;
+        }
+
+        $this->valor = $numero;
+    }
+
+    // doc
+    /**
+     * Pega o número do gênero
+     *
+     * @return int|string Número do gênero
+     */
+    public function numero(): int|string
+    {
+        return $this->numero;
+    }
+}
