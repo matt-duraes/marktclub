60a5ef0ca597496e72ba247c41b1e31705273381
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-08-09 09:38:39 -0300

    Fix: Corrigindo o valor_pib do comercial

--- views/pages/painel/comercial_empresa/Models/VisualizarModel.php
+++ views/pages/painel/comercial_empresa/Models/VisualizarModel.php
@@ -12,13 +12,15 @@ final class VisualizarModel implements PainelVisualizarRetornoInterface
     {
         $dado->valor_usuario = '';
         if ($dado->tipo_pagamento == TipoPagamento::USUARIO) {
-            $dado->valor_usuario = 'R$ ' . $dado->contrato_valor;
+            $dado->valor_usuario = !empty($dado->contrato_valor) ? 'R$ ' . $dado->contrato_valor : '';
         }
-        $dado->valor_pago = 'R$ ' . $dado->valor_pago;
+        $dado->valor_pago = !empty($dado->valor_pago) ? 'R$ ' . $dado->valor_pago : '';
 
         $dado->email_dia = $this->converterDiaSemana($dado->email_dia);
         $dado->whatsapp_dia = $this->converterDiaSemana($dado->whatsapp_dia);
         $dado->rede_social_dia = $this->converterDiaSemana($dado->rede_social_dia);
+
+        $dado->valor_pib = '';
         return $dado;
     }
 
