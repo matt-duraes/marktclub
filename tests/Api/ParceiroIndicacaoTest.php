828108329951b0234a1c672e92ef389a95f53107
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-08-21 09:40:13 -0300

    test: adicionado testes para indicação de parceiro

--- tests/Api/ParceiroIndicacaoTest.php
+++ tests/Api/ParceiroIndicacaoTest.php
@@ -32,11 +32,51 @@ class ParceiroIndicacaoTest extends Clube
             ->post('/parceiro-indicacao')
             ->array();
 
-        $this->idIndicacaoNovoParceiro = $dado['dado']['id'];
+        $this->idIndicacaoNovoParceiro = $dado['dado']['id'] ?? '';
 
         return $this
             ->checkStatus(201)
             ->checkIndiceIgual('status', 'sucesso');
+    }
+
+    public function salvarIndidicacaoNovoParceiroSemNomeTest(): ParceiroIndicacaoTest
+    {
+        $this->api('parceiro_indicacao:salvar');
+        $this
+            ->Curl
+            ->header(['Authorization' => $this->pegarToken()])
+            ->body([
+                'email'             => $this->email(),
+                'telefone'          => $this->telefone(),
+                'mensagem'          => 'Mensagem de teste',
+                'status'            => valorAleatorio($this->statusValidos)
+            ])
+            ->post('/parceiro-indicacao');
+
+        return $this
+            ->checkStatus(400)
+            ->checkIndiceIgual('status', 'erro');
+    }
+
+    public function salvarIndicacaoNomeInvalidoTest(): ParceiroIndicacaoTest
+    {
+        $this->api('parceiro_indicacao:salvar');
+        $dado = $this
+            ->Curl
+            ->header(['Authorization' => $this->pegarToken()])
+            ->body([
+                'nome'              => $this->nome(),
+                'email'             => $this->email(),
+                'telefone'          => $this->telefone(),
+                'mensagem'          => 'Mensagem de teste',
+                'status'            => valorAleatorio($this->statusValidos)
+            ])
+            ->post('/parceiro-indicacao')
+            ->array();
+
+        return $this
+            ->checkStatus(400)
+            ->checkIndiceIgual('status', 'erro');
     }
 
     public function listarIndicacoesNovoParceiroTest(): ParceiroIndicacaoTest
