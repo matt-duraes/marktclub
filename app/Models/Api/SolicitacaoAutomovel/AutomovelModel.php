f442b4a39a0cd4e86cacbbaed118266f13fcc680
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-08-23 18:18:35 -0300

    Feat: Criando models da solicitacao Automovel

---
+++ app/Models/Api/SolicitacaoAutomovel/AutomovelModel.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models\Api\SolicitacaoAutomovel;
+
+use ORM\ORM;
+use stdClass;
+use Modules\Data;
+use Modules\Pagina;
+use Modules\Quantidade;
+use System\Trait\Model\OrdemTrait;
+use App\Classes\Solicitacao\Status;
+use System\Trait\Model\PaginaTrait;
+use System\Trait\Model\QuantidadeTrait;
+use System\Interface\ModelListarInterface;
+use App\Classes\SolicitacaoAutomovel\Ordem;
+
+final class AutomovelModel extends ORM implements ModelListarInterface
+{
+    use PaginaTrait;
+    use QuantidadeTrait;
+    use OrdemTrait;
+
+    protected string $ormTabela = TABELA_SOLICITACAO_AUTOMOVEL;
+
+    public function __construct(
+        protected Pagina $pagina = new Pagina(null),
+        protected Quantidade $quantidade = new Quantidade(null),
+        protected Data $dataCriacaoDe = new Data(null),
+        protected Data $dataCriacaoAte = new Data(null),
+        protected Status $status = new Status(null),
+        protected ?string $empresa = null,
+        protected Ordem $ordem = new Ordem(null)
+    ) {
+        parent::__construct();
+        $this->validarDado();
+    }
+
+    public function listarDados(): stdClass
+    {
+        $dado = $this
+            ->campo(['uuid'])
+            ->pagina($this->pegarPagina(), $this->pegarQuantidade())
+            ->order($this->pegarOrdem())
+            ->read();
+
+        $dado->lista = $this->montarDado($dado->lista);
+        return $dado;
+    }
+
+    private function montarDado($dado)
+    {
+        $retorno = [];
+        foreach ($dado as $r) {
+            $retorno[] = (object)[
+                'id' => $r->uuid,
+            ];
+        }
+        return $retorno;
+    }
+
+    private function validarDado()
+    {
+    }
+}
