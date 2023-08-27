46699d9ddbcfd46d5c38d6a855e0ead0b5f98103
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-08-27 18:12:56 -0300

    Feat: Colocando artigo na noticia

--- app/Classes/PublicacaoNoticia/Tipo.php
+++ app/Classes/PublicacaoNoticia/Tipo.php
@@ -7,12 +7,14 @@ use Status\Status as StatusStatus;
 class Tipo extends StatusStatus
 {
     public const NOTICIA = 'noticia';
+    public const ARTIGO = 'artigo';
 
     public function __construct(
         protected string|int|null $valor = null
     ) {
         parent::__construct([
-            self::NOTICIA       => 'Notícia',
+            self::NOTICIA => 'Notícia',
+            self::ARTIGO  => 'Artigo',
         ]);
     }
 }
