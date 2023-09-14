476c0b3d37419ab7e15d3dca97a6eb4afc1aed88
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-09-14 11:36:58 -0300

    fix: corrigindo env para todos os sites

--- app/Controllers/Api/SamsungController.php
+++ app/Controllers/Api/SamsungController.php
@@ -5,14 +5,12 @@ namespace App\Controllers\Api;
 use Http\Request;
 use Http\Response;
 use Controller\Controller;
-use App\Models\Api\Samsung\LogModel;
 use App\Models\Api\Samsung\ValidarModel;
 
 final class SamsungController extends Controller
 {
     public function PostValidar(Request $request, $user = null, $validate = null): Response
     {
-        new LogModel();
         $Validar = new ValidarModel($request->getGet('code'));
         return new Response(
             json: $Validar->validar(),
