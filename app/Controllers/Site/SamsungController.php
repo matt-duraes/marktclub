7eb39fe77867cfd22339f8b93faf6ec7a9e634ea
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-09-21 18:18:55 -0300

    Feat: Criando sistema de popup no FW

--- app/Controllers/Site/SamsungController.php
+++ app/Controllers/Site/SamsungController.php
@@ -3,13 +3,18 @@
 namespace App\Controllers\Site;
 
 use Controller\Controller;
+use App\Models\Site\BannerModel;
+use App\Models\Site\Samsung\BuscarModel;
 
 final class SamsungController extends Controller
 {
     public function index()
     {
+        $dado = (new BuscarModel())->buscar();
         return view('samsung', [
-            'menu' => 'samsung'
+            'menu'   => 'samsung',
+            'banner' => (new BannerModel())->samsung(),
+            'dado'   => $dado
         ]);
     }
 }
--- views/pages/site/samsung/js/all.js
+++ views/pages/site/samsung/js/all.js
@@ -1,1 +1,17 @@
 // @template "site"
+// @system "Popup"
+
+const atualizarEmail = bloco => {
+    const botao = bloco.querySelector('.botao_atualizar_email');
+    botao.addEventListener('click', () => {
+        ppe('teste');
+    });
+};
+window.addEventListener('load', () => {
+    const botaoAbrirPopupAtualizar = $('#botao_samsung_atualizar');
+    const PopupAtualizar = new Popup('atualizar-dado', $('#bloco_atualizar_email'), true, true, atualizarEmail);
+
+    botaoAbrirPopupAtualizar.addEventListener('click', () => {
+        PopupAtualizar.abrir();
+    });
+});
