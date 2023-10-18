011cf6886401ea12f9eb0021b1d6070e13408b7b
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-10-18 17:22:52 -0300

    feat(analytics-dado): adicionado mais dados ao banco de dados

--- database/analytics_usuario/dado.php
+++ database/analytics_usuario/dado.php
@@ -1,29 +1,53 @@
 <?php
 
 $data = hoje();
-return [
-    ['id' => '1', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '36', 'data_acesso' => dataRemover($data, 8, 'dia')],
-    ['id' => '2', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '44', 'data_acesso' => dataRemover($data, 8, 'dia')],
-    ['id' => '3', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '42', 'data_acesso' => dataRemover($data, 8, 'dia')],
-    ['id' => '4', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '43', 'data_acesso' => dataRemover($data, 7, 'dia')],
-    ['id' => '5', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '49', 'data_acesso' => dataRemover($data, 7, 'dia')],
-    ['id' => '6', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '51', 'data_acesso' => dataRemover($data, 7, 'dia')],
-    ['id' => '7', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '46', 'data_acesso' => dataRemover($data, 6, 'dia')],
-    ['id' => '8', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '58', 'data_acesso' => dataRemover($data, 6, 'dia')],
-    ['id' => '9', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '55', 'data_acesso' => dataRemover($data, 6, 'dia')],
-    ['id' => '10', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '71', 'data_acesso' => dataRemover($data, 5, 'dia')],
-    ['id' => '11', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '57', 'data_acesso' => dataRemover($data, 5, 'dia')],
-    ['id' => '12', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '58', 'data_acesso' => dataRemover($data, 5, 'dia')],
-    ['id' => '13', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '67', 'data_acesso' => dataRemover($data, 4, 'dia')],
-    ['id' => '14', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '55', 'data_acesso' => dataRemover($data, 4, 'dia')],
-    ['id' => '15', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '53', 'data_acesso' => dataRemover($data, 4, 'dia')],
-    ['id' => '16', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '60', 'data_acesso' => dataRemover($data, 3, 'dia')],
-    ['id' => '17', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '60', 'data_acesso' => dataRemover($data, 3, 'dia')],
-    ['id' => '18', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '53', 'data_acesso' => dataRemover($data, 3, 'dia')],
-    ['id' => '19', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '60', 'data_acesso' => dataRemover($data, 2, 'dia')],
-    ['id' => '20', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '52', 'data_acesso' => dataRemover($data, 2, 'dia')],
-    ['id' => '21', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '44', 'data_acesso' => dataRemover($data, 2, 'dia')],
-    ['id' => '22', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '2', 'usuario_cpf' => '70490840183', 'usuario_nome' => 'Higor Ferreira Araújo', 'quantidade' => '39', 'data_acesso' => dataRemover($data, 1, 'dia')],
-    ['id' => '23', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '1', 'usuario_cpf' => '91956278206', 'usuario_nome' => 'Murilo Oliveira Rodrigues', 'quantidade' => '53', 'data_acesso' => dataRemover($data, 1, 'dia')],
-    ['id' => '24', 'id_admin_empresa' => '1', 'id_usuario_cliente' => '3', 'usuario_cpf' => '79050390641', 'usuario_nome' => 'Tereza Nascimento Teixeira', 'quantidade' => '49', 'data_acesso' => dataRemover($data, 1, 'dia')]
-];
+$dado = [];
+
+for ($e = 1; $e <= 50; $e++) {
+    $usuario1 = [
+        'nome'               => nomeCompletoAleatorio(),
+        'cpf'                => cpfAleatorio(),
+        'id_usuario_cliente' => $e * numeroAleatorio(1, 1000),
+    ];
+    $usuario2 = [
+        'nome'               => nomeCompletoAleatorio(),
+        'cpf'                => cpfAleatorio(),
+        'id_usuario_cliente' => $e * numeroAleatorio(1, 1000),
+    ];
+    $usuario3 = [
+        'nome'               => nomeCompletoAleatorio(),
+        'cpf'                => cpfAleatorio(),
+        'id_usuario_cliente' => $e * numeroAleatorio(1, 1000),
+    ];
+
+    for ($i = 0; $i <= 10; $i++) {
+        $dado[] = [
+            'id_admin_empresa'   => $e,
+            'id_usuario_cliente' => $usuario1['id_usuario_cliente'],
+            'usuario_cpf'        => $usuario1['cpf'],
+            'usuario_nome'       => $usuario1['nome'],
+            'quantidade'         => rand(1, 100),
+            'data_acesso'        => dataRemover($data, $i, 'dia')
+        ];
+
+        $dado[] = [
+            'id_admin_empresa'   => $e,
+            'id_usuario_cliente' => $usuario2['id_usuario_cliente'],
+            'usuario_cpf'        => $usuario2['cpf'],
+            'usuario_nome'       => $usuario2['nome'],
+            'quantidade'         => rand(1, 100),
+            'data_acesso'        => dataRemover($data, $i, 'dia')
+        ];
+
+        $dado[] = [
+            'id_admin_empresa'   => $e,
+            'id_usuario_cliente' => $usuario3['id_usuario_cliente'],
+            'usuario_cpf'        => $usuario3['cpf'],
+            'usuario_nome'       => $usuario3['nome'],
+            'quantidade'         => rand(1, 100),
+            'data_acesso'        => dataRemover($data, $i, 'dia')
+        ];
+    }
+}
+
+return $dado;
