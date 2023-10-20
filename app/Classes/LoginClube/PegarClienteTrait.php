ab66fb6170d5662ce30c142fa0db28305729c5b7
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-10-20 10:55:01 -0300

    refactor(api-carteirinha): trocado nome da rota e adicionado criptografia

--- app/Classes/LoginClube/PegarClienteTrait.php
+++ app/Classes/LoginClube/PegarClienteTrait.php
@@ -6,14 +6,14 @@ use Helpers\OrmHelper;
 
 trait PegarClienteTrait
 {
-    private function pegarCliente(array $where)
+    private function pegarCliente(array $where, array $campoAdicional = [])
     {
         return (new OrmHelper(TABELA_USUARIO_CLIENTE))->pegarUltimoRegistro(
             where: $where,
-            campo: [
+            campo: array_merge([
                 'id', 'uuid', 'salt', 'cpf', 'nome', 'imagem', 'email_pessoal', 'email_trabalho', 'tipo',
                 'grupo', 'primeiro_acesso', 'mudar_senha', 'data_termo', 'data_criacao', 'data_atualizacao'
-            ],
+            ], $campoAdicional),
             retorno: 'object'
         );
     }
