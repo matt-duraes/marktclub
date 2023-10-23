5408ba3416dd77c14802c77e186b855d6660b2a2
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-10-23 09:31:56 -0300

    refactor(tests-analytics): refatorado testes do analytics

---
+++ tests/Backend/Api/Trait/AnalyticsTrait.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Api\Trait;
+
+trait AnalyticsTrait
+{
+    private string $id1 = '14afa776394ada4be23be6acf7e3259e';
+    private string $id2 = '0ffc5c56b99f81ca0edea8bdf524b688';
+
+    private function getBody(string|null $id = null)
+    {
+        return [
+            'de'      => dataRemover(hoje(), 7, 'dias'),
+            'ate'     => hoje(),
+            'empresa' => $id,
+        ];
+    }
+
+    private function fazerRequest(
+        string|null $id = null,
+        array|null $body = null
+    ): array {
+        if (!$body) {
+            $body = $this->getBody($id);
+        }
+        $dado = $this
+            ->Curl
+            ->json($body)
+            ->get($this->uri)
+            ->array()['dado'] ?? [];
+
+        $this
+            ->checkStatus(200)
+            ->checkIndiceIgual('status', 'sucesso');
+
+        return $dado;
+    }
+
+    private function pegarSoma($key)
+    {
+        $soma = [];
+
+        foreach ($this->dadoEmpresa1 as $item) {
+            $dispositivo = $item[$key];
+            $soma[$dispositivo] = $item['total'];
+        }
+
+        foreach ($this->dadoEmpresa2 as $item) {
+            $dispositivo = $item[$key];
+            if (isset($soma[$dispositivo])) {
+                $soma[$dispositivo] += $item['total'];
+            } else {
+                $soma[$dispositivo] = $item[$key];
+            }
+        }
+
+        return $soma;
+    }
+
+    private function buscarSemEmpresa()
+    {
+        $body = $this->getBody();
+        $body['empresa'] = '';
+
+        $this
+            ->Curl
+            ->json($body)
+            ->get($this->uri);
+
+        return $this
+            ->checkStatus(200)
+            ->checkIndiceIgual('status', 'sucesso');
+    }
+}
