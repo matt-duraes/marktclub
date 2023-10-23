c905187ca16e3d137ab6ec43f712f920a472b6da
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-10-23 09:23:01 -0300

    refactor: Refatorando a carterinha

--- views/pages/painel/carteirinha/config/index.php
+++ views/pages/painel/carteirinha/config/index.php
@@ -6,7 +6,7 @@ use App\Classes\Carteirinha\Status;
 $Painel = new PainelConfig\Index('carteirinha', new Ordem());
 
 $Painel
-    ->campo('empresa.nome', 'Empresa', 'normal', permissao: \App\Classes\UsuarioCliente\Helper::PERMISSAO_EMPRESA)
+    ->campo('empresa.nome', 'Empresa', 'normal')
     ->dataCriacao()
     ->dataAtualizacao()
     ->status('status', 'Status', new Status());
