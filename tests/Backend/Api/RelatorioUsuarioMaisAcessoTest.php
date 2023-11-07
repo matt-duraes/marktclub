d1a9fc31877df4140d6dab365a30be088fb3cd40
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-07 09:22:51 -0300

    test(relatorio-usuario): criado testes

---
+++ tests/Backend/Api/RelatorioUsuarioMaisAcessoTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Api;
+
+use Tests\Api\Trait\AnalyticsTrait;
+use Tests\Tests;
+
+final class RelatorioUsuarioMaisAcessoTest extends Tests
+{
+    use AnalyticsTrait;
+
+    private string $uri = '/relatorio/usuario-mais-acesso';
+    private array $dadoEmpresa1;
+    private array $dadoEmpresa2;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->api('relatorio_acesso:listar');
+    }
+
+    public function buscarSemEmpresaTest()
+    {
+        return $this->buscarSemEmpresa();
+    }
+
+    public function buscarPrimeiraEmpresaTest()
+    {
+        $this->dadoEmpresa1 = $this->fazerRequest($this->id1);
+        return $this;
+    }
+
+    public function buscarSegundaEmpresaTest()
+    {
+        $this->dadoEmpresa2 = $this->fazerRequest($this->id2);
+        return $this;
+    }
+
+    public function buscarAsDuasEmpresasTest()
+    {
+        $body = $this->getBody();
+        $body['empresa'] = [
+            $this->id1,
+            $this->id2,
+        ];
+
+        $dado = $this->fazerRequest(body: $body);
+        $dispositivosSoma = $this->pegarSoma('usuario', crypto: true);
+
+        foreach ($dado as $k => $d) {
+            $usuario = $this->cryptDecode($d['usuario']);
+            $this->checkIgual($d['total'], $dispositivosSoma[$usuario]);
+        }
+
+        return $this;
+    }
+}
