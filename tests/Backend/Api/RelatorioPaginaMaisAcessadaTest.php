c04c7a5d1629a37890b06d38b04fa95584063610
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-07 09:23:47 -0300

    test(relatorio-pagina): criado teste

---
+++ tests/Backend/Api/RelatorioPaginaMaisAcessadaTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Api;
+
+use Tests\Api\Trait\AnalyticsTrait;
+use Tests\Tests;
+
+final class RelatorioPaginaMaisAcessadaTest extends Tests
+{
+    use AnalyticsTrait;
+
+    private string $uri = '/relatorio/pagina-mais-acessada';
+    private array $dadoEmpresa1;
+    private array $dadoEmpresa2;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->api('relatorio_acesso:listar');
+    }
+
+    public function buscarSemEmpresaTest()
+    {
+        return $this->buscarSemEmpresa();
+    }
+
+    public function buscarPrimeiraEmpresaTest()
+    {
+        $this->dadoEmpresa1 = $this->fazerRequest($this->id1);
+        return $this;
+    }
+
+    public function buscarSegundaEmpresaTest()
+    {
+        $this->dadoEmpresa2 = $this->fazerRequest($this->id2);
+        return $this;
+    }
+
+    public function buscarAsDuasEmpresasTest()
+    {
+        $body = $this->getBody();
+        $body['empresa'] = [
+            $this->id1,
+            $this->id2,
+        ];
+
+        $dado = $this->fazerRequest(body: $body);
+        $dispositivosSoma = $this->pegarSoma('pagina');
+
+        foreach ($dado as $k => $d) {
+            $this->checkIgual($d['total'], $dispositivosSoma[$d['pagina']]);
+        }
+
+        return $this;
+    }
+}
