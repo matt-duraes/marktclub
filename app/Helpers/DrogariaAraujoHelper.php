ed3e6c323d97a38d60993639b861714eec99846c
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-11-08 17:35:37 -0300

    improvement(api-drogaria-araujo): adicionando rota no postman e realizando ajustes para a api

---
+++ app/Helpers/DrogariaAraujoHelper.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Helpers;
+
+use Erro\Erro;
+
+class DrogariaAraujoHelper
+{
+    private string $codigoPlano;
+
+    /**
+     * @throws Erro
+     */
+    public function __construct()
+    {
+        $envsAlfa = [
+            'DROGARIA_ARAUJO_PLANO' => env('DROGARIA_ARAUJO_PLANO')
+        ];
+
+        foreach ($envsAlfa as $index => $value) {
+            if (empty($value)) {
+                throw new Erro(
+                    "Variável de ambiente $index não foi seta ou está vazia",
+                    'Variáveis de Ambiente',
+                    "A variável de ambiente $index deve ser preenchida corretamente"
+                );
+            } elseif (!is_string($value)) {
+                throw new Erro(
+                    "Esperavamos um valor do tipo STRING na variável de ambiente $index",
+                    'Tipagem da variável de ambiente',
+                    "A variável de ambiente $index deve ser do tipo STRING"
+                );
+            }
+        }
+
+        $this->codigoPlano = env('DROGARIA_ARAUJO_PLANO');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCodigoPlano(): string
+    {
+        return $this->codigoPlano;
+    }
+}
