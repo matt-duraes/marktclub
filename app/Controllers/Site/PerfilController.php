703fb08caa703f2f288f194975458631b69f0dca
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-10 15:46:00 -0300

    fix(popup-login): botao de alterar senha

--- app/Controllers/Site/PerfilController.php
+++ app/Controllers/Site/PerfilController.php
@@ -70,9 +70,9 @@ final class PerfilController extends Controller
         return view(
             'perfil.carteirinha',
             [
-                'dado' => $campos_carteirinha,
+                'dado'          => $campos_carteirinha,
                 'dados_cliente' => $dados_cliente->usuario,
-                'data_emissao' => $data_emissao,
+                'data_emissao'  => $data_emissao,
                 'data_validade' => $data_validade
             ]
         );
