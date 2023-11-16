688d8e1abcb163dcbed3844287ff487f7d49bbb5
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-11-16 11:02:09 -0300

    improvement(api-dependente): adicionando funcao de reenviar email

--- app/Models/Api/UsuarioDependente/DependenteModel.php
+++ app/Models/Api/UsuarioDependente/DependenteModel.php
@@ -2,60 +2,96 @@
 
 namespace App\Models\Api\UsuarioDependente;
 
-use ORM\ORM;
-use Http\Request;
 use App\Classes\UsuarioCliente\Helper;
 use App\Classes\UsuarioCliente\Status;
 use App\Models\Api\UsuarioCliente\ClienteEntity;
+use Erro\Excecao;
+use ORM\ORM;
 
 final class DependenteModel extends ORM
 {
     protected string $ormTabela = TABELA_USUARIO_CLIENTE;
 
+    /**
+     * @param string|null $usuario
+     *
+     * @throws Excecao
+     */
     public function __construct(
-        private ?Request $request = null
+        private readonly ?string $usuario = null
     ) {
+        $this->validarDados();
         parent::__construct();
     }
 
-    /*
-    |--------------------------------------------------------------------------
-    | LISTA USUÁRIOS
-    |--------------------------------------------------------------------------
-    */
-    public function listar()
+    /**
+     * @return void
+     * @throws Excecao
+     */
+    private function validarDados(): void
     {
-        validarUuid($this->request->usuario);
-        $titular = $this->pegarTitular();
+        validarUuid($this->usuario);
+    }
 
-        $lista = $this->campo(['cod', 'nome', 'email_pessoal', 'email_trabalho', 'status'])->where([
-            ['titular', $titular],
-            ['status', 'in', Helper::STATUS_LIBERADO]
-        ])->order('id', 'ASC')->limit(0, 5)->read();
+    /**
+     * @return array
+     * @throws Excecao
+     */
+    public function listarDados(): array
+    {
+        $lista = $this
+            ->campo([
+                'cod', 'nome', 'email_pessoal',
+                'email_trabalho', 'status'
+            ])
+            ->where([
+                ['titular', $this->pegarTitular()],
+                ['status', 'in', Helper::STATUS_LIBERADO]
+            ])
+            ->order('id')
+            ->limit(0, 5)
+            ->read();
 
         return $this->montarRetorno($lista);
     }
 
+    /**
+     * @return int|bool
+     * @throws Excecao
+     */
     private function pegarTitular(): int|bool
     {
         $Cliente = new ClienteEntity(validarToken: false);
         $Cliente->buscar([
-            ['cod', $this->request->usuario],
+            ['cod', $this->usuario],
             ['status', 'in', Helper::STATUS_LIBERADO]
         ]);
         return $Cliente->get('id');
     }
 
-    private function montarRetorno(array $dado): array
+    /**
+     * @param array $dependentes
+     *
+     * @return array
+     */
+    private function montarRetorno(array $dependentes): array
     {
         $retorno = [];
         $Status = new Status();
-        foreach ($dado as $r) {
+        foreach ($dependentes as $dependente) {
+            $email = 'Sem e-mail';
+
+            if (!empty($dependente->email_pessoal)) {
+                $email = $dependente->email_pessoal;
+            } elseif (!empty($dependente->email_trabalho)) {
+                $email = $dependente->email_trabalho;
+            }
+
             $retorno[] = [
-                'id'     => $r->cod,
-                'nome'   => $r->nome,
-                'email'  => !empty($r->email_pessoal) ? $r->email_pessoal : $r->email_trabalho,
-                'status' => $Status->indice($r->status)
+                'id' => $dependente->cod,
+                'nome' => $dependente->nome ?? 'Sem nome',
+                'email' => $email,
+                'status' => $Status->indice($dependente->status)
             ];
         }
         return $retorno;
