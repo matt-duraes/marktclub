4ff34cc191cb5679111034384d1b7d3b47cf102c
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-11-21 16:20:26 -0300

    feat(api-subempresa): preparando controller

--- app/Controllers/Api/ComercialSubempresaController.php
+++ app/Controllers/Api/ComercialSubempresaController.php
@@ -2,18 +2,149 @@
 
 namespace App\Controllers\Api;
 
+use App\Classes\ComercialSubempresa\Ordem;
+use App\Classes\ComercialSubempresa\Status;
+use App\Models\Api\ComercialSubempresa\SelectModel;
+use App\Models\Api\ComercialSubempresa\SubempresaEntity;
+use App\Models\Api\ComercialSubempresa\SubempresaModel;
+use Controller\Controller;
+use Erro\Excecao;
 use Http\Request;
 use Http\Response;
-use Controller\Controller;
-use App\Models\Api\ComercialSubempresa\SelectModel;
+use Modules\Pagina;
+use Modules\Quantidade;
+use System\Interface\ControllerAtualizarInterface;
+use System\Interface\ControllerBuscarInterface;
+use System\Interface\ControllerDeletarInterface;
+use System\Interface\ControllerListarInterface;
+use System\Interface\ControllerSalvarInterface;
+use System\Interface\ControllerSelectInterface;
 
-final class ComercialSubempresaController extends Controller
+final class ComercialSubempresaController extends Controller implements
+    ControllerSelectInterface,
+    ControllerBuscarInterface,
+    ControllerListarInterface,
+    ControllerSalvarInterface,
+    ControllerAtualizarInterface,
+    ControllerDeletarInterface
 {
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     * @throws Excecao
+     */
     public function getSelect(Request $request): Response
     {
         $Empresa = new SelectModel($request);
-        $dado = $Empresa->listarSelect();
+        return mensagemSucesso($Empresa->listarSelect());
+    }
 
-        return mensagemSucesso($dado);
+    /**
+     * @param string $id
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function getBuscar(string $id): Response
+    {
+        $SubempresaEntity = new SubempresaEntity();
+        $SubempresaEntity->uuid(
+            $id,
+            mensagem: '',
+            titulo: 'Nã'
+        );
+        return $this->retornoSucesso($SubempresaEntity);
+    }
+
+    /**
+     * @param SubempresaEntity $subempresaEntity
+     * @param int              $status
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    private function retornoSucesso(SubempresaEntity $subempresaEntity, int $status = 200): Response
+    {
+        return mensagemSucesso(
+            pegarPropriedadeDaEntity(
+                $subempresaEntity,
+                lista: [
+                    'nome', 'documento_cnpj', 'status'
+                ]
+            ),
+            $status
+        );
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function getListar(Request $request): Response
+    {
+        $SubempresaModel = new SubempresaModel(
+            new Pagina($request->pagina),
+            new Quantidade($request->quantidade),
+            new Ordem($request->ordem),
+            $request->titulo,
+            $request->empresa,
+            new Status($request->status)
+        );
+        return mensagemSucesso($SubempresaModel->listarDados());
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function postSalvar(Request $request): Response
+    {
+        $SubempresaEntity = new SubempresaEntity();
+        $SubempresaEntity->set(lista: $request->dado());
+        $SubempresaEntity->salvar();
+        return $this->retornoSucesso($SubempresaEntity, 201);
+    }
+
+    /**
+     * @param Request $request
+     * @param string  $id
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function putAtualizar(Request $request, string $id): Response
+    {
+        $SubempresaEntity = new SubempresaEntity();
+        $SubempresaEntity->uuid(
+            $id,
+            mensagem: '',
+            titulo: 'Nã'
+        );
+        $SubempresaEntity->set(lista: $request->dado());
+        $SubempresaEntity->salvar();
+        return new Response(status: 204);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return Response
+     * @throws Excecao
+     */
+    public function deleteDeletar(string $id): Response
+    {
+        $SubempresaEntity = new SubempresaEntity();
+        $SubempresaEntity->uuid(
+            $id,
+            mensagem: '',
+            titulo: 'Nã'
+        );
+        $SubempresaEntity->destruir();
+        return new Response(status: 204);
     }
 }
