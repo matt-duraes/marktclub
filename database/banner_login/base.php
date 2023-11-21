de17600ab1ff613f5ea12fb7b0cc7c5df943e774
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-21 09:57:50 -0300

    fix(Banner-login): arrumado tipo da coluna

--- database/banner_login/base.php
+++ database/banner_login/base.php
@@ -5,7 +5,7 @@ use DataBase\DataBase;
 return (new DataBase())
     ->id()
     ->uuid()
-    ->int('id_admin_empresa')
+    ->json('id_admin_empresa')
     ->text('url_1')
     ->text('url_2')
     ->text('url_3')
