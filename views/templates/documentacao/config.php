a4f6ba9cd4329e805403e7757d7cbd8766ef81ee
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2023-12-20 09:17:57 -0300

    feat: colocando logo da youhuul

--- views/templates/documentacao/config.php
+++ views/templates/documentacao/config.php
@@ -1,5 +1,5 @@
 <?php
 
-define('LINK_PRODUCAO', 'https://apiv4.markt.club');
-define('LINK_HOMOLOGACAO', 'https://apiv4hml.markt.club');
+define('LINK_PRODUCAO', 'https://apiv4.youhuul.com');
+define('LINK_HOMOLOGACAO', 'https://apiv4hml.youhuul.com');
 define('SCOPE', sessao('DOCUMENTACAO.scope', padrao: []));
