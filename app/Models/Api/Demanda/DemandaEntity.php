1fc2cdfc91503d1c006dbf1b017f7d2b55439059
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-12-21 16:40:22 -0300

    refactor(demanda): ajustes de indentacao e propriedades

--- app/Models/Api/Demanda/DemandaEntity.php
+++ app/Models/Api/Demanda/DemandaEntity.php
@@ -2,22 +2,38 @@
 
 namespace App\Models\Api\Demanda;
 
-use ORM\Entity;
-use Modules\Data;
-use Modules\Botao;
-use Modules\DataHora;
-use Helpers\OrmHelper;
-use App\Classes\DemandaDado\Area;
-use App\Classes\DemandaDado\Tipo;
-use App\Classes\DemandaDado\Status;
-use System\Classes\PainelHistorico\Acao;
 use ApiModel\PainelHistorico\HistoricoEntity;
+use App\Classes\DemandaDado\Area;
+use App\Classes\DemandaDado\Status;
+use App\Classes\DemandaDado\Tipo;
 use App\Models\Api\Demanda\Trait\EmpresaTrait;
+use Erro\Erro;
+use Erro\Excecao;
+use Helpers\OrmHelper;
+use Modules\Botao;
+use Modules\Data;
+use Modules\DataHora;
+use ORM\Entity;
+use System\Classes\PainelHistorico\Acao;
 
 final class DemandaEntity extends Entity
 {
     use EmpresaTrait;
 
+    public array $arquivo = [];
+    public string $equipe;
+    public array $seguindo = [];
+    public Botao $com_prazo;
+    public Data $data_entrega;
+    public DataHora $data_entrega_real;
+    public int $ordem;
+    public int $id_admin_empresa;
+    public Status $status;
+    public int $id_usuario_equipe;
+    public string $titulo;
+    public string|array $empresa;
+    public Tipo $tipo;
+    public Area $area;
     protected string $ormTabela = TABELA_DEMANDA_DADO;
     protected array $ormBuscar = [
         'id_admin_empresa', 'id_usuario_equipe', 'titulo', 'tipo', 'status', 'seguindo',
@@ -37,67 +53,14 @@ final class DemandaEntity extends Entity
         status|Status|vazio|valido
         data_entrega|Data da entrega|valido
     ';
-    public array $arquivo = [];
-    public string $equipe;
-    public array $seguindo = [];
-    public Botao $com_prazo;
-    public Data $data_entrega;
-    public DataHora $data_entrega_real;
-    public int $ordem;
-    public int $id_admin_empresa;
-    public Status $status;
-    public int $id_usuario_equipe;
-    public string $titulo;
-    public string|array $empresa;
-    public Tipo $tipo;
-    public Area $area;
 
-    /*
-    |--------------------------------------------------------------------------
-    | BUSCAR
-    |--------------------------------------------------------------------------
-    */
-    protected function regraPosBuscar()
-    {
-        $this->equipe = (new OrmHelper(TABELA_USUARIO_EQUIPE))->pegarUuidPeloId($this->id_usuario_equipe);
-        $this->empresa = (new OrmHelper(TABELA_COMERCIAL_EMPRESA))->pegarUuidPeloId($this->id_admin_empresa);
-        if ($this->com_prazo->valor() != 'sim') {
-            $this->data_entrega = new Data('');
-        }
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | INSERT
-    |--------------------------------------------------------------------------
-    */
-    protected function regraInsert()
-    {
-        $this->com_prazo = new Botao('nao');
-        $this->status = new Status(1);
-        $this->id_usuario_equipe = TOKEN['usuario']->id;
-        $this->ordem = 999;
-        $this->pegarIdEmpresa();
-    }
-
-    protected function regraSalvar()
-    {
-        if ($this->com_prazo->valor() == 'sim' && $this->data_entrega->vazio()) {
-            mensagemErro('Campo obrigatório!', 'A data de entrega é obrigatória.');
-        }
-    }
-
-    protected function getId()
-    {
-        return $this->prop('id');
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | CANCELAR
-    |--------------------------------------------------------------------------
-    */
-    public function cancelar(string $motivo)
+    /**
+     * @param string $motivo
+     *
+     * @return void
+     * @throws Excecao
+     */
+    public function cancelar(string $motivo): void
     {
         new CancelarTarefaModel(Demanda: $this);
 
@@ -112,7 +75,7 @@ final class DemandaEntity extends Entity
         $Historico->salvar();
     }
 
-    public function seguir()
+    public function seguir(): void
     {
         $id = $this->pegarIdUsuario();
         if (empty($id) || in_array($id, $this->seguindo)) {
@@ -121,7 +84,15 @@ final class DemandaEntity extends Entity
         $this->seguindo[] = $id;
     }
 
-    public function seguirParar()
+    private function pegarIdUsuario()
+    {
+        if (!defined('TOKEN') || !array_key_exists('usuario', TOKEN)) {
+            return '';
+        }
+        return TOKEN['usuario']->uuid;
+    }
+
+    public function seguirParar(): void
     {
         $id = $this->pegarIdUsuario();
         if (empty($id)) {
@@ -132,11 +103,42 @@ final class DemandaEntity extends Entity
         $this->seguindo = array_keys($seguindo);
     }
 
-    private function pegarIdUsuario()
+    protected function regraPosBuscar(): void
     {
-        if (!defined('TOKEN') || !array_key_exists('usuario', TOKEN)) {
-            return '';
+        $this->equipe = (new OrmHelper(TABELA_USUARIO_EQUIPE))->pegarUuidPeloId($this->id_usuario_equipe);
+        $this->empresa = (new OrmHelper(TABELA_COMERCIAL_EMPRESA))->pegarUuidPeloId($this->id_admin_empresa);
+        if ($this->com_prazo->valor() != 'sim') {
+            $this->data_entrega = new Data('');
         }
-        return TOKEN['usuario']->uuid;
+    }
+
+    protected function regraInsert(): void
+    {
+        $this->com_prazo = new Botao('nao');
+        $this->status = new Status(1);
+        $this->id_usuario_equipe = TOKEN['usuario']->id;
+        $this->ordem = 999;
+        $this->pegarIdEmpresa();
+    }
+
+    /**
+     * @return void
+     * @throws Excecao
+     */
+    protected function regraSalvar(): void
+    {
+        if ($this->com_prazo->valor() == 'sim' && $this->data_entrega->vazio()) {
+            mensagemErro('Campo obrigatório!', 'A data de entrega é obrigatória.');
+        }
+    }
+
+    /**
+     * @return mixed
+     * @throws Excecao
+     * @throws Erro
+     */
+    protected function getId(): mixed
+    {
+        return $this->prop('id');
     }
 }
