bf53db6a72bc3ae67ab0014f5bb71d433d11f642
Mateus Duraes <mateusduraessilva@gmail.com>
2023-12-22 10:10:28 -0300

    fix: ajuste acao voltar pagina no mapa

--- views/templates/site/js/mapa.js
+++ views/templates/site/js/mapa.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () => {
     if (botaoLojaProxima.length == 0) {
         return;
     }
+
     botaoLojaProxima.forEach(botao => {
         botao.addEventListener('click', () => {
             if ('geolocation' in navigator) {
@@ -12,10 +13,18 @@ window.addEventListener('load', () => {
             }
         });
     });
+
+    let abortController; // Variável para armazenar o controller para abortar a solicitação
+
     const pegarLocalizacao = () => {
+        abortController = new AbortController(); // Criar um novo AbortController
+
         Loading.show();
+
         navigator.geolocation.getCurrentPosition(
             position => {
+                Loading.hide();
+                abortController.abort(); // Cancelar a solicitação quando a localização é obtida
                 window.location.assign(
                     LINK +
                         '/convenios?latitude=' +
@@ -39,10 +48,16 @@ window.addEventListener('load', () => {
                     'Ocorreu um erro ao pegar sua localização, verifique suas permissões no navegador e tente novamente.',
                     '!'
                 );
-            }
+            },
+            { signal: abortController.signal } // Passar o signal do AbortController para a solicitação
         );
     };
-});
-window.addEventListener('beforeunload', () => {
-    Loading.hide();
+
+    // Adicionar um listener para o evento popstate (quando o usuário clica no botão de voltar do navegador)
+    window.addEventListener('popstate', () => {
+        if (abortController) {
+            abortController.abort(); // Cancelar a solicitação se o usuário clicar no botão de voltar
+        }
+        Loading.hide();
+    });
 });
