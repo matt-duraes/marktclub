0e3c342715b06db95d313922d1308b1c07428800
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-01-15 15:52:05 -0300

    fix(ParceiroLoj): removido bloco do filtrar

--- views/pages/painel/parceiro_loja/config/filtrar.php
+++ views/pages/painel/parceiro_loja/config/filtrar.php
@@ -5,15 +5,13 @@ use App\Classes\ParceiroLoja\Status;
 
 $Painel = new PainelConfig\Filtrar('parceiro_loja');
 
-$Painel->bloco(function () use ($Painel) {
-    $Painel
-        ->select(name: 'empresa', label: 'Empresa', lista: 'empresa', permissao: Helper::PERMISSAO_EMPRESA)
-        ->select(
-            name: 'status',
-            label: 'Status',
-            lista: (new Status())->select('Escolha uma opção')
-        );
-});
+$Painel
+    ->select(name: 'empresa', label: 'Empresa', lista: 'empresa', permissao: Helper::PERMISSAO_EMPRESA)
+    ->select(
+        name: 'status',
+        label: 'Status',
+        lista: (new Status())->select('Escolha uma opção')
+    );
 
 $Painel->replace('status', (new Status())->select());
 
