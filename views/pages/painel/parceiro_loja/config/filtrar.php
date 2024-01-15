84da8289241a64fdab84ee169b3ead56cf038852
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-01-15 13:13:44 -0300

    fix(ParceiroLoja): arrumado filtro

--- views/pages/painel/parceiro_loja/config/filtrar.php
+++ views/pages/painel/parceiro_loja/config/filtrar.php
@@ -5,15 +5,15 @@ use App\Classes\ParceiroLoja\Status;
 
 $Painel = new PainelConfig\Filtrar('parceiro_loja');
 
-$Painel
-    ->select(name: 'empresa', label: 'Empresa', lista: 'empresa', permissao: Helper::PERMISSAO_EMPRESA)
-    ->input(name: 'titulo', titulo: 'Título', label: 'Título', placeholder: 'Digite um título')
-    ->select(
-        name: 'status',
-        titulo: 'Status',
-        label: 'Status',
-        lista: (new Status())->select('Escolha uma opção')
-    );
+$Painel->bloco(function () use ($Painel) {
+    $Painel
+        ->select(name: 'empresa', label: 'Empresa', lista: 'empresa', permissao: Helper::PERMISSAO_EMPRESA)
+        ->select(
+            name: 'status',
+            label: 'Status',
+            lista: (new Status())->select('Escolha uma opção')
+        );
+});
 
 $Painel->replace('status', (new Status())->select());
 
