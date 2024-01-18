e1e9574a313b4c6a3b1708c76265d603e5d3706e
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-01-18 16:12:56 -0300

    fix: colocando samsung para funcionar com qualquer usuario

--- app/Helpers/ClubeApiHelper.php
+++ app/Helpers/ClubeApiHelper.php
@@ -10,8 +10,9 @@ class ClubeApiHelper extends ApiHelper
     protected string $idUsuario;
     public CryptHelper $Crypt;
 
-    public function __construct(string $scope = null)
-    {
+    public function __construct(
+        private ?string $scope = null
+    ) {
         $token = is_null($scope) ? true : false;
         parent::__construct(scope: $scope, token: $token);
         $this->idUsuario = sessao('USUARIO.id');
@@ -20,21 +21,51 @@ class ClubeApiHelper extends ApiHelper
 
     private function setarCryptHelper()
     {
+        if (!empty($this->scope)) {
+            $this->pegarChaveViaScope();
+            return;
+        }
         if (!sessaoExiste('CRYPT_CHAVE_PUBLICA') || empty(sessao('CRYPT_CHAVE_PUBLICA'))) {
-            $chave = $this
-                ->get('/admin/chave-publica')
-                ->object()->dado->chave ?? '';
-            sessao('CRYPT_CHAVE_PUBLICA', $chave);
+            $chavePublica = $this->pegarChavePublica();
+            sessao('CRYPT_CHAVE_PUBLICA', $chavePublica);
         }
         if (!sessaoExiste('CRYPT_CHAVE_PRIVADA') || empty(sessao('CRYPT_CHAVE_PRIVADA'))) {
-            $chave = $this
-                ->get('/admin/chave-privada')
-                ->object()->dado->chave ?? '';
+            $chave = $this->pegarChavePrivada();
             sessao('CRYPT_CHAVE_PRIVADA', $chave);
         }
         $this->Crypt = new CryptHelper(
             chavePublica: sessao('CRYPT_CHAVE_PUBLICA'),
             chavePrivada: sessao('CRYPT_CHAVE_PRIVADA')
         );
+    }
+
+    private function pegarChaveViaScope()
+    {
+        if (!sessaoExiste('CRYPT_CHAVE_PUBLICA_SCOPE') || empty(sessao('CRYPT_CHAVE_PUBLICA_SCOPE'))) {
+            $chavePublica = $this->pegarChavePublica();
+            sessao('CRYPT_CHAVE_PUBLICA_SCOPE', $chavePublica);
+        }
+        if (!sessaoExiste('CRYPT_CHAVE_PRIVADA_SCOPE') || empty(sessao('CRYPT_CHAVE_PRIVADA_SCOPE'))) {
+            $chavePrivada = $this->pegarChavePrivada();
+            sessao('CRYPT_CHAVE_PRIVADA_SCOPE', $chavePrivada);
+        }
+        $this->Crypt = new CryptHelper(
+            chavePublica: sessao('CRYPT_CHAVE_PUBLICA_SCOPE'),
+            chavePrivada: sessao('CRYPT_CHAVE_PRIVADA_SCOPE')
+        );
+    }
+
+    private function pegarChavePublica()
+    {
+        return $this
+            ->get('/admin/chave-publica')
+            ->object()->dado->chave ?? '';
+    }
+
+    private function pegarChavePrivada()
+    {
+        return $this
+            ->get('/admin/chave-privada')
+            ->object()->dado->chave ?? '';
     }
 }
