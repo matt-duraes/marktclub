9ddbd104dd3e202432c8d4877d096ae95e72fad5
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-01-26 11:16:15 -0300

    improvement(painel): melhorias gerais

--- views/pages/painel/saude_contratacao/config/index.php
+++ views/pages/painel/saude_contratacao/config/index.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Classes\PublicacaoPagina\Ordem;
+use App\Classes\Saude\Ordem;
 use App\Classes\Saude\Status;
 
 $Painel = new PainelConfig\Index('saude_contratacao', new Ordem());
+
 return $Painel
-    ->campo('nome', 'nome', 'grande')
+    ->campo('nome', 'Nome', 'grande')
     ->campo('cpf', 'CPF', 'normal')
     ->dataCriacao()
     ->status('status', 'Status', new Status());
--- views/pages/painel/usuario_indicacao/config/filtrar.php
+++ views/pages/painel/usuario_indicacao/config/filtrar.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Classes\UsuarioIndicacao\Helper;
 use App\Classes\UsuarioIndicacao\Status;
 
 $Painel = new PainelConfig\Filtrar('usuario_indicacao');
@@ -11,7 +12,7 @@ $Painel
         titulo: 'Empresa',
         label: 'Empresa',
         placeholder: 'Empresa',
-        permissao: \App\Classes\UsuarioIndicacao\Helper::PERMISSAO_EMPRESA
+        permissao: Helper::PERMISSAO_EMPRESA
     )
     ->input(name: 'nome', titulo: 'Nome do indicado', label: 'Nome', placeholder: 'Digite um nome')
     ->input(name: 'email', titulo: 'E-mail do indicado', label: 'E-mail', placeholder: 'Digite um e-mail')
