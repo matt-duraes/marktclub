8ede9543dffe982d00b908c0205055f601a69112
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-01-29 10:07:14 -0300

    fix(UsuarioCliente): adicionado validação de buscar imagem

--- app/Models/Api/UsuarioCliente/Trait/EntityBuscarTrait.php
+++ app/Models/Api/UsuarioCliente/Trait/EntityBuscarTrait.php
@@ -16,7 +16,9 @@ trait EntityBuscarTrait
             $this->contratoSiape = $this->trabalho_empresa->numero() . $this->siape . '341201';
         }
 
-        $this->imagem = arquivoPublico('usuario_cliente', $this->imagem_arquivo);
+        if (!$this->imagem_arquivo->vazio()) {
+            $this->imagem = arquivoPublico('usuario_cliente', $this->imagem_arquivo);
+        }
 
         if ($this->validarToken) {
             $Pagamento = new PagamentoModel();
