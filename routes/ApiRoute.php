0c41d30d6597ca97695ac0d80f5f7c17e435a404
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-02-05 09:30:16 -0300

    fix(ParceiroLoja): arrumado filtro

--- routes/ApiRoute.php
+++ routes/ApiRoute.php
@@ -1260,7 +1260,7 @@ Route
             ::request([
                 'pagina', '!quantidade', '!categoria', '!subcategoria', '!estabelecimento',
                 '!pesquisa', '!tipo', '!status', '!ordem', '!favorito', '!mais_acessado',
-                '!latitude', '!longitude', '!estado'
+                '!latitude', '!longitude', '!estado', '!empresa'
             ], 'json')
             ::get('/parceiro-loja');
         Route
