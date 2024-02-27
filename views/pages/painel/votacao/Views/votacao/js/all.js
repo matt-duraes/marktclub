97baec188b544b41d021b673c9e77bcc8d6554dd
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-02-27 15:41:08 -0300

    feat: criando painel de pergunta

--- views/pages/painel/votacao/Views/votacao/js/all.js
+++ views/pages/painel/votacao/Views/votacao/js/all.js
@@ -1,15 +1,24 @@
 // @template "painel"
 // @system "DragDrop"
+// @system "Popup"
+
 window.addEventListener('load', () => {
-    manipularResposta();
-});
-const manipularResposta = () => {
+    const botaoSalvarPergunta = $('#botao_pergunta_salvar');
     const botaoAdicionarResposta = $('#botao_adicionar_resposta');
     const blocoRespostaPadrao = $('#bloco_linha_resposta');
     const blocoRespostaLoading = $('#bloco_resposta_zero');
     const blocoRespostaZero = $('#bloco_resposta_zero');
-    const blocoResposta = $('#bloco_lista_resposta');
-    new DragDrop().bloco(blocoResposta).item('.linha').botao('i.drag').iniciar();
+    const blocoRespostaLista = $('#bloco_lista_resposta');
+
+    const inputRespostaTitulo = $('input[name="pergunta_titulo"]');
+    const inputRespostaTexto = $('textarea[name="pergunta_texto"]');
+    const inputRespostaTipo = $('input[name="pergunta_tipo"]');
+    const inputRespostaNulo = $('input[name="pergunta_nulo"]');
+
+    const botaoPergunta = $('#botao_adicionar_pergunta');
+    const PopupSalvar = new Popup('Adicionar pergunta', 'bloco_pergunta_add');
+
+    new DragDrop().bloco(blocoRespostaLista).item('.linha').botao('i.drag').iniciar();
 
     botaoAdicionarResposta.evento('click', () => {
         if (botaoAdicionarResposta.classe('loading', '?')) {
@@ -17,25 +26,50 @@ const manipularResposta = () => {
         }
         const clone = blocoRespostaPadrao.clonar();
         const titulo = $('[name="resposta_titulo"]', clone);
-        blocoResposta.inicio(clone);
+        blocoRespostaLista.inicio(clone);
         titulo.focus();
         blocoRespostaZero.sumir();
     });
-    blocoResposta.evento('click', e => {
+    blocoRespostaLista.evento('click', e => {
         const target = e.target;
-        if (!target.classe('deletar', '?') && !target.closest('.deletar')) {
+        if (target.classe('deletar', '?') || target.closest('.deletar')) {
+            removerResposta(target.closest('.linha'));
             return;
+        } else if (target.classe('editar', '?') || target.closest('.editar')) {
+            editarPergunta();
         }
-        removerResposta(target.closest('.linha'));
     });
     const removerResposta = linha => {
         if (!linha) {
             return;
         }
         linha.remover();
-        if ($$('.linha', blocoResposta).length > 0) {
+        if ($$('.linha', blocoRespostaLista).length > 0) {
             return;
         }
         blocoRespostaZero.aparecer();
     };
-};
+
+    const editarPergunta = async () => {
+        botaoAdicionarResposta.classe('loading', true);
+        blocoRespostaZero.sumir();
+        blocoRespostaLoading.aparecer();
+        const resposta = await fetch(LINK + '/', {});
+    };
+
+    const resetarPergunta = () => {
+        blocoRespostaLista.html('');
+        inputRespostaTitulo.valor('');
+        inputRespostaTexto.valor('');
+        inputRespostaTipo.valor('');
+        inputRespostaNulo.valor(false);
+    };
+
+    botaoPergunta.evento('click', () => {
+        botaoAdicionarResposta.classe('loading', false);
+        blocoRespostaZero.sumir();
+        blocoRespostaLoading.aparecer();
+        resetarPergunta();
+        PopupSalvar.abrir();
+    });
+});
