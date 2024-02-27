23975acac75bc4ca5720f68574eb55e179db8316
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-02-27 10:15:27 -0300

    feat: adicionando novo campo

--- views/pages/painel/painel_config/config/add.php
+++ views/pages/painel/painel_config/config/add.php
@@ -24,6 +24,7 @@ $Painel->coluna(callback: function () use ($Painel) {
     $Painel->fieldsetCheckbox(
         titulo: 'Recursos do Painel',
         callback: function () use ($Painel) {
+            $Painel->checkbox(name: 'configuracao[]', label: 'Nome', value: 'nome');
             $Painel->checkbox(name: 'configuracao[]', label: 'Perfil', value: 'perfil');
             $Painel->checkbox(name: 'configuracao[]', label: 'Bloquear Tela', value: 'bloquear');
             $Painel->checkbox(name: 'configuracao[]', label: 'Agenda Google', value: 'agenda');
