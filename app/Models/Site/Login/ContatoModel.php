04feb7d7b76f11e2ecd0e323002e7205ef0600bc
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-03-19 15:34:05 -0300

    improvement: adicionado nome no contato

--- app/Models/Site/Login/ContatoModel.php
+++ app/Models/Site/Login/ContatoModel.php
@@ -13,7 +13,7 @@ final class ContatoModel
 
     public function buscarDados($tipo)
     {
-        return (new ApiHelper('contato:listar'))
+        $dado = (new ApiHelper('contato:listar'))
             ->json([
                 'vinculo' => $this->parceiro,
                 'local'   => 'clube',
@@ -21,5 +21,25 @@ final class ContatoModel
             ])
             ->get('/contato')
             ->array()['dado'] ?? [];
+        return $this->montarRetorno($dado);
+    }
+
+    private function montarRetorno($dado)
+    {
+        if (empty($dado)) {
+            return [];
+        }
+        $retorno = [];
+        foreach ($dado as $d) {
+            $retorno[] = [
+                'local'     => $d['local'],
+                'tipo'      => $d['tipo'],
+                'outro'     => $d['outro'],
+                'nome'      => $d['nome'],
+                'documento' => $d['documento'],
+                'valor'     => $d['contato'] ? $d['contato'] . ' - ' . $d['valor'] : $d['valor'],
+            ];
+        }
+        return $retorno;
     }
 }
