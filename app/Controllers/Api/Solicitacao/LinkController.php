c58fa5eea21dd51b42e7338f7f7da190536259e4
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-03-21 18:18:04 -0300

    feat: colocando limite de voucher

--- app/Controllers/Api/Solicitacao/LinkController.php
+++ app/Controllers/Api/Solicitacao/LinkController.php
@@ -4,28 +4,29 @@ namespace App\Controllers\Api\Solicitacao;
 
 use Http\Response;
 use Controller\Controller;
+use App\Models\Api\Solicitacao\Link\HashModel;
+use App\Models\Api\Solicitacao\Link\LimiteModel;
 use App\Models\Api\Solicitacao\Link\ResgatarEntity;
 
 final class LinkController extends Controller
 {
     public function confirmar(string $hash)
     {
-        $dado = base64Decode($hash, true);
-        if (!$dado) {
-            mensagemStatus(404);
-        }
+        $Hash = new HashModel($hash);
+        $Limite = new LimiteModel($Hash);
         return view('redirecionar', [
-            'parceiro' => (object)$dado['parceiro'],
-            'clube'    => (object)$dado['clube'],
+            'parceiro' => $Hash->parceiro,
+            'clube'    => $Hash->clube,
+            'saldo'    => $Limite->saldo,
+            'usado'    => $Limite->usado,
+            'lista'    => $Limite->lista,
             'hash'     => $hash
         ]);
     }
 
     public function redirecionar(string $hash)
     {
-        $Link = new ResgatarEntity(
-            dado: base64Decode($hash, true)
-        );
+        $Link = new ResgatarEntity(new HashModel($hash));
         return new Response(url: $Link->link);
     }
 }
