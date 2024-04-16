4fa7eeb1945ccac1d6c51c66a62729a84083b0e3
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-04-16 17:21:43 -0300

    fix: correcao fechamento com x do filtro

--- src/Painel/Controllers/AppController.php
+++ src/Painel/Controllers/AppController.php
@@ -574,10 +574,9 @@ final class AppController extends PadraoController
         $indice = $request->indice;
         $ordem = $request->existe('ordem') && !empty($request->ordem)
             && $indice != 'ordem' ? 'ordem=' . $request->ordem : '';
-        $filtro = $request->existe('filtro') && !empty($request->filtro) ? base64Decode($request->filtro) : '';
-
-        $validar = is_array($filtro) && $filtro;
-        if ($validar && $indice != 'ordem' && array_key_exists($indice, $filtro)) {
+        $filtro = $request->existe('filtro') && !empty($request->filtro) ? base64Decode($request->filtro) : [];
+        $filtro = is_array($filtro) ? $filtro : [];
+        if ($filtro && $indice != 'ordem' && array_key_exists($indice, $filtro)) {
             unset($filtro[$indice]);
         }
         $filtroFinal = [];
@@ -590,7 +589,7 @@ final class AppController extends PadraoController
         $filtro = $filtroFinal;
 
         $url = LINK . '/app/' . $app;
-        if ($indice == 'ordem' && $validar) {
+        if ($indice == 'ordem' && $filtro) {
             $url = LINK . '/app/' . $app . '?filtro=' . $request->filtro;
         } elseif (empty($filtro) && !empty($ordem)) {
             $url = LINK . '/app/' . $app . '?' . $ordem;
