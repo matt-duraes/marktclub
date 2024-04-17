7e9ba27dec6e0fad961567ef7b13e355b8580b49
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-04-17 11:24:35 -0300

    improvement: melhorado tratamento de erro da busca de favorito

--- app/Models/Site/Loja/FavoritoModel.php
+++ app/Models/Site/Loja/FavoritoModel.php
@@ -21,7 +21,7 @@ final class FavoritoModel extends ClubeApiHelper
     {
         $dado = $this->get('/parceiro-favorito')->object();
         if (!chaveExiste('dado', $dado) || !$dado->dado) {
-            $dado->dado = [];
+            return [];
         }
         $retorno = [];
         foreach ($dado->dado as $r) {
