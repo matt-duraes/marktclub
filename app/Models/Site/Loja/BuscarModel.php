7947e35300c8128bca675b4a77ae3ecc11aad9a4
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-04-16 21:00:04 -0300

    fix: corrigindo link do cashback

--- app/Models/Site/Loja/BuscarModel.php
+++ app/Models/Site/Loja/BuscarModel.php
@@ -47,7 +47,7 @@ final class BuscarModel extends ClubeApiHelper
             'procedimento'       => $r->tipo_procedimento,
             'capa_desktop'       => $r->imagem_capa_desktop,
             'capa_mobile'        => $r->imagem_capa_mobile,
-            'link'               => $r->link_site,
+            'link'               => $this->gerarLink($tipo, $r->link_site),
             'url'                => $r->url,
             'desconto'           => $r->desconto,
             'arquivo'            => $this->buscarArquivo($tipo, $r->arquivo_clube),
@@ -56,6 +56,14 @@ final class BuscarModel extends ClubeApiHelper
             'email'              => $r->existe_email == 'sim',
             'telefone'           => $r->existe_telefone == 'sim',
         ];
+    }
+
+    private function gerarLink(string $tipo, string $link = null)
+    {
+        if ($tipo != TipoLoja::CASHBACK || empty($link)) {
+            return $link;
+        }
+        return $link . '&clickref=' . sessao('USUARIO.id');
     }
 
     private function buscarArquivo($tipo, $arquivo): array
