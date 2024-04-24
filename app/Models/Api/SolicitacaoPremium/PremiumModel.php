715967886b1b532c5941cd00c7e04045c5d6f88c
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-04-24 09:28:34 -0300

    improvement(VoucherPremium): buscar tipo_loja na constante

--- app/Models/Api/SolicitacaoPremium/PremiumModel.php
+++ app/Models/Api/SolicitacaoPremium/PremiumModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Api\SolicitacaoPremium;
 
+use App\Classes\ParceiroLoja\TipoLoja;
 use ORM\ORM;
 use Erro\Excecao;
 use Http\Request;
@@ -52,7 +53,7 @@ final class PremiumModel extends ORM
             ->campo([
                 'id', 'titulo', 'limite_voucher'
             ])
-            ->where(['tipo_loja', 4])
+            ->where(['tipo_loja', (new TipoLoja(TipoLoja::PREMIUM))->numero()])
             ->read();
         return $this->montarRetorno($dado);
     }
