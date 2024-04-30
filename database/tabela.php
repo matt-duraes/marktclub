2aa27c80b3947e909658e21bf7ad3e8dfba3e556
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-04-30 14:21:56 -0300

    fix: colocando tabela

--- database/tabela.php
+++ database/tabela.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 define('TABELA_ALBUM_DADO', 'album_dado');
 define('TABELA_ALBUM_FOTO', 'album_foto');
@@ -8,6 +8,7 @@ define('TABELA_ANALYTICS_DADO_USUARIO', 'analytics_dado_usuario');
 define('TABELA_ANALYTICS_DISPOSITIVO', 'analytics_dispositivo');
 define('TABELA_ANALYTICS_LOJA', 'analytics_loja');
 define('TABELA_ANALYTICS_LOJA_EQUIPE', 'analytics_loja_equipe');
+define('TABELA_ANALYTICS_LOJA_VENDA', 'analytics_loja_venda');
 define('TABELA_ANALYTICS_NAVEGADOR', 'analytics_navegador');
 define('TABELA_ANALYTICS_OS', 'analytics_os');
 define('TABELA_ANALYTICS_PAGINA', 'analytics_pagina');
