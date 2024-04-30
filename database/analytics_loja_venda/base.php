72f34e6b8390c70648f466ec3664e7899050a5ee
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-04-30 14:20:10 -0300

    fix: colocando base_loja_venda de volta

---
+++ database/analytics_loja_venda/base.php
@@ -0,0 +1,12 @@
+<?php
+
+return (new \DataBase\DataBase())
+    ->id()
+    ->uuid()
+    ->int('id_admin_empresa')->tamanho(9)
+    ->int('id_parceiro_loja')->tamanho(9)
+    ->int('numero_transacao')->tamanho(9)
+    ->dinheiro('valor_venda')
+    ->date('data_relatorio')
+    ->dataCriacao()
+    ->dataAtualizacao();
