0dc902091903ade6857731b021bc32e6515e94ca
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-05-09 17:11:15 -0300

    feat: colocando imagem mobile para a live

--- views/pages/painel/publicacao_live/config/salvar.php
+++ views/pages/painel/publicacao_live/config/salvar.php
@@ -2,7 +2,8 @@
 
 return [
     'salvar' => [
-        'titulo', 'titulo_interno', 'texto', 'imagem_site', 'imagem_restrito', 'link',
-        'permissao_restrita', 'permissao_site', 'link_restrito', 'data_inicio', 'data_final', 'status'
+        'titulo', 'titulo_interno', 'texto', 'imagem_site_desktop', 'imagem_site_mobile',
+        'imagem_restrito_desktop', 'imagem_restrito_mobile', 'link', 'permissao_restrita',
+        'permissao_site', 'link_restrito', 'data_inicio', 'data_final', 'status'
     ]
 ];
