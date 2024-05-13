5ef58806d52a185e0e722a70f695727e6dd1a8dc
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-05-13 08:45:28 -0300

    fix: criando api de live

--- routes/ApiRoute.php
+++ routes/ApiRoute.php
@@ -2759,8 +2759,8 @@ Route
             ::middleware(TokenMiddleware::class, 'scope', ['publicacao_live:atualizar'])
             ::request([
                 'titulo', 'titulo_interno', 'texto', 'imagem_site_desktop', 'imagem_site_mobile',
-                'imagem_restrito_desktop', 'imagem_restrito_mobile', 'link', 'permissao_restrita',
-                'permissao_site', 'link_restrito', 'data_inicio', 'data_final', 'status'
+                'imagem_restrito_desktop', 'imagem_restrito_mobile', 'link', 'incorporar', 'permissao_restrita',
+                'permissao_site', 'link_restrito', 'data_inicio', 'data_final', 'botao_texto', 'status'
             ])
             ::put('/publicacao-live/{id}');
     });
