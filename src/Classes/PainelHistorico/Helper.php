cdf07de9e221780c2ff4ad7b59adaf332c9d6696
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-06-14 15:02:47 -0300

    improvement: arrumado parametros da rota

--- src/Classes/PainelHistorico/Helper.php
+++ src/Classes/PainelHistorico/Helper.php
@@ -5,7 +5,7 @@ namespace System\Classes\PainelHistorico;
 final class Helper
 {
     public const GET_PARAMETRO = [
-        'pagina', 'app', 'relacionado', '!data_de', '!data_ate', '!pesquisa',
+        'pagina', '!app', 'relacionado', '!data_de', '!data_ate', '!pesquisa',
         '!historico_app'
     ];
 }
