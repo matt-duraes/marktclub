9f53d1f50f6aa5dccffda5fdd2c98b3737540877
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-06-20 17:16:21 -0300

    improvement: melhorado sistema de testes

--- cypress.config.js
+++ cypress.config.js
@@ -6,12 +6,15 @@ module.exports = defineConfig({
     fixturesFolder: 'tests/Frontend/config/fixtures',
     screenshotsFolder: 'tests/Frontend/config/screenshots',
     e2e: {
-        baseUrl: process.env.CYPRESS_BASE_URL,
         specPattern: 'tests/Frontend/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: 'tests/Frontend/config/support/e2e.js',
         experimentalRunAllSpecs: true,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+    },
+    env: {
+        urlClube: process.env.CYPRESS_BASE_URL_CLUBE,
+        urlPainel: process.env.CYPRESS_BASE_URL_PAINEL,
     },
 });
