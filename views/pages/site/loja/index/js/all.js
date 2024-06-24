6922def5b9ab4abd7ae1880537221b918f69b5eb
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-06-24 14:14:54 -0300

    feat: corrigindo header fixo no desktop

--- views/pages/site/loja/index/js/all.js
+++ views/pages/site/loja/index/js/all.js
@@ -7,27 +7,6 @@
 // @system "Esqueleto"
 // @system "Popup"
 
-let scrollAtual = window.scrollY;
-let scrollUltimaAcao = window.scrollY;
-const headerPrincipalFixo = $('#header_principal_fixo');
-const scrollMenuPrincipal = () => {
-    const topo = window.scrollY;
-    const aparecido = headerPrincipalFixo.classe('aparecer', '?');
-    const diferenca = topo > scrollUltimaAcao ? topo - scrollUltimaAcao : scrollUltimaAcao - topo;
-    const podeMudar = diferenca > 30;
-    if ((scrollAtual < topo || topo < 80) && aparecido && podeMudar) {
-        headerPrincipalFixo.classe('aparecer', false);
-        scrollUltimaAcao = topo;
-    } else if (scrollAtual >= topo && topo > 80 && !aparecido && podeMudar) {
-        headerPrincipalFixo.classe('aparecer', true);
-        scrollUltimaAcao = topo;
-    }
-    scrollAtual = topo;
-};
-window.addEventListener('scroll', function () {
-    scrollMenuPrincipal();
-});
-
 window.addEventListener('load', () => {
     const blocoPopup = $('#popup_medicamento');
     if (!blocoPopup) {
