f3e6559ee66f8b49ad561b8c0e31d743d372e298
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-06-25 15:31:17 -0300

    fix: finalizando sistema de assinatura

--- src/Painel/App/assinatura/Views/index/js/form.js
+++ src/Painel/App/assinatura/Views/index/js/form.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
-    const botao = $('#botao_copiar_assinatura');
+    const botaoCopiar = $('#botao_copiar_assinatura');
+    const botaoAbrir = $('#botao_abrir_assinatura');
     const blocoNome = $('#bloco_assinatura .nome');
     const blocoSeparador = $('#bloco_assinatura .separador');
     const blocoCargoSigla = $('#bloco_assinatura .cargo strong');
@@ -21,39 +22,64 @@ window.addEventListener('load', () => {
         #input_assinatura_celular, #input_assinatura_telefone, #input_assinatura_email
     `);
 
-    const copiarAssinatura = (nome, cargoSigla, cargoNome, celular, telefone, email) => {
-        if (
-            vazio(nome) ||
-            (vazio(cargoSigla) && vazio(cargoNome)) ||
-            (vazio(celular) && vazio(telefone)) ||
-            vazio(email)
-        ) {
-            Alerta.notificacao('Preencha os campos obrigatórios para continuar.', false);
-            return;
-        }
+    const pegarNomeFinal = (nome, cargoSigla, cargoNome) => {
         let nomeFinal = nome;
         if (!vazio(cargoSigla) && !vazio(cargoNome)) {
-            nomeFinal += ` | <b style="color: #FF6F00;">${cargoSigla}</b> - ${cargoNome}`;
+            nomeFinal += ` | <b style="color: #FF6F00;">${cargoSigla}</b>${cargoNome}`;
         } else if (!vazio(cargoSigla)) {
             nomeFinal += ` | <span style="color: #999">${cargoSigla}</span>`;
         } else if (!vazio(cargoNome)) {
             nomeFinal += ` | <span style="color: #999">${cargoNome}</span>`;
         }
+        return nomeFinal;
+    };
 
+    const pegarTelefoneFinal = (telefone, celular) => {
         let telefoneFinal = '+55 (61) ';
         if (!vazio(celular)) {
             telefoneFinal += `<b>${celular}</b>`;
         }
         if (!vazio(celular) && !vazio(telefone)) {
-            telefoneFinal += ' | ' + telefone;
+            telefoneFinal += telefone;
         } else if (!vazio(telefone)) {
             telefoneFinal += `<b>${telefone}</b>`;
         }
+        return telefoneFinal;
+    };
+
+    const validarCampoObrigatorio = () => {
+        if (
+            vazio(inputNome.valor()) ||
+            (vazio(inputCargoNome.valor()) && vazio(inputCargoSigla.valor())) ||
+            (vazio(inputTelefone.valor()) && vazio(inputCelular.valor())) ||
+            vazio(inputEmail.valor())
+        ) {
+            Alerta.notificacao('Preencha os campos obrigatórios para continuar.', false);
+            return false;
+        }
+        return true;
+    };
+
+    const gerarLink = (nome, cargoSigla, cargoNome, celular, telefone, email) => {
+        botaoAbrir.attr(
+            'href',
+            `${LINK}/assinatura/html?nome=${encodeURI(nome)}&cargoSigla=${encodeURI(cargoSigla)}&cargoNome=${encodeURI(
+                cargoNome
+            )}&telefone=${encodeURI(telefone)}&celular=${encodeURI(celular)}&email=${encodeURI(email)}`
+        );
+    };
+
+    const copiarAssinatura = (nome, cargoSigla, cargoNome, celular, telefone, email) => {
+        if (!validarCampoObrigatorio()) {
+            return;
+        }
+        const nomeFinal = pegarNomeFinal(nome, cargoSigla, cargoNome);
+        const telefoneFinal = pegarTelefoneFinal(telefone, celular);
 
         const texto = assinaturaHtml()
             .replace('{{NOME}}', nomeFinal)
             .replace('{{TELEFONE}}', telefoneFinal)
-            .replace('{{EMAIL}}', email + '@youhuul.com.br');
+            .replace('{{EMAIL}}', !vazio(email) ? email + '@youhuul.com.br' : '');
 
         navigator.clipboard.writeText(texto);
         Alerta.notificacao('Assinatura copiada com sucesso!', true);
@@ -89,17 +115,25 @@ window.addEventListener('load', () => {
         blocoCargoNome.texto(cargoNome);
         blocoCelular.texto(celular);
         blocoTelefone.texto(telefone);
-        blocoEmail.texto(email + '@youhuul.com.br');
+        blocoEmail.texto(!vazio(email) ? email + '@youhuul.com.br' : '');
 
         if (copiar) {
             copiarAssinatura(nome, cargoSigla, cargoNome, celular, telefone, email);
         }
+        gerarLink(nome, cargoSigla, cargoNome, celular, telefone, email);
     };
+
     inputLista.evento('keyup', () => {
         montarPrevia();
     });
 
-    botao.evento('click', () => {
+    botaoCopiar.evento('click', () => {
         montarPrevia(true);
+    });
+    botaoAbrir.evento('click', e => {
+        if (!validarCampoObrigatorio()) {
+            e.preventDefault();
+            return;
+        }
     });
 });
