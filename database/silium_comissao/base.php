5fed4c6414c50e8ab34b98e41346ea6bcc5cefb2
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-07-03 16:52:01 -0300

    fix: corrigindo falta da coluna na comissao

--- database/silium_comissao/base.php
+++ database/silium_comissao/base.php
@@ -5,6 +5,7 @@ use DataBase\DataBase;
 return (new DataBase())
     ->id()
     ->uuid()
+    ->int('id_admin_empresa')->tamanho(9)->relacionado(TABELA_COMERCIAL_EMPRESA, 'id')
     ->int('id_usuario_cliente')->tamanho(9)->relacionado(TABELA_USUARIO_CLIENTE, 'id')
     ->varchar('parceiro')->tamanho(100)
     ->float('valor_compra')
