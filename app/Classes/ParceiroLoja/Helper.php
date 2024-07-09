d49ef34dd06eced40fe5474254dafe3309c3badf
Thiago Crepequer <thiago.crepequer@hotmail.com>
2024-07-09 11:47:56 -0300

    fix: arrumado obrigatoriedade do parametro quantidade

--- app/Classes/ParceiroLoja/Helper.php
+++ app/Classes/ParceiroLoja/Helper.php
@@ -7,7 +7,7 @@ final class Helper
     public const CRIPTOGRAFAR = [];
     public const PERMISSAO_EMPRESA = 'parceiro_loja_empresa';
     public const PARAMETROS_LISTAR = [
-        'pagina', 'quantidade', '!categoria', '!subcategoria', '!tipo_estabelecimento',
+        'pagina', '!quantidade', '!categoria', '!subcategoria', '!tipo_estabelecimento',
         '!pesquisa', '!titulo', '!tipo_loja', '!status', '!ordem', '!favorito', '!mais_acessado',
         '!latitude', '!longitude', '!endereco_estado', '!empresa', '!equipe', '!convenio_direto',
         '!convenio', '!data_criacao_de', '!data_criacao_ate', '!data_publicacao_de',
