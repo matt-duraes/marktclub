c27ae90caeb68dcd8462f9818c210c38fa098c09
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-07-19 14:39:49 -0300

    fix: ajuste de config silium

--- app/Models/Api/SiliumDeposito/SiliumDepositoEntity.php
+++ app/Models/Api/SiliumDeposito/SiliumDepositoEntity.php
@@ -8,7 +8,6 @@ use App\Classes\SiliumDeposito\TipoOperacao;
 use App\Classes\SiliumDeposito\TipoResgate;
 use App\Models\Api\ConstrutorClube\ConstrutorEntity;
 use App\Models\Api\SiliumSaldo\SiliumSaldoEntity;
-use Erro\Erro;
 use Erro\Excecao;
 use Helpers\EmailHelper;
 use Helpers\OrmHelper;
@@ -66,13 +65,12 @@ class SiliumDepositoEntity extends Entity
     private function pegarConfiguracoes(): void
     {
         $OrmHelper = new OrmHelper(TABELA_SILIUM_CONFIG);
-        try {
-            $configs = $OrmHelper->pegarUltimoRegistro(
-                ['id_admin_empresa', TOKEN['empresa']->id],
-                ['desconto', 'pontuacao_minima_resgate'],
-                'object'
-            );
-        } catch (Excecao) {
+        $configs = $OrmHelper->pegarUltimoRegistro(
+            ['id_admin_empresa', TOKEN['empresa']->id],
+            ['desconto', 'pontuacao_minima_resgate'],
+            'object'
+        );
+        if (empty($configs->desconto)) {
             $configs = $OrmHelper->pegarUltimoRegistro(
                 ['id_admin_empresa', 1],
                 ['desconto', 'pontuacao_minima_resgate'],
