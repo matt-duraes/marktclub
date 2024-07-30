eae0b5943727ef36c21dc87d6a9f0d2918fe39d1
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-07-30 15:54:14 -0300

    feat(api-parceiro-externo): adicionando contato ao visualizar

--- app/Classes/PublicacaoArquivo/Tipo.php
+++ app/Classes/PublicacaoArquivo/Tipo.php
@@ -6,13 +6,30 @@ use Status\Status as StatusStatus;
 
 class Tipo extends StatusStatus
 {
-    public const GERAL = 'geral';
+    public const EMPRESA = [
+        'geral'   => [
+            'lista'  => [
+                'geral' => 'Geral'
+            ],
+            'numero' => [1]
+        ],
+        'intelis' => [
+            'lista'  => [
+                'estatuto'             => 'Estatuto',
+                'notas-juridicas'      => 'Notas Jurídicas',
+                'pareceres'            => 'Pareceres',
+                'relatorios-processos' => 'Relatórios Processos'
+            ],
+            'numero' => [2, 3, 4, 5]
+        ]
+    ];
 
+    /**
+     * @param string|int|null $valor
+     */
     public function __construct(
         protected string|int|null $valor = null
     ) {
-        parent::__construct([
-            self::GERAL => 'Geral'
-        ]);
+        parent::__construct(empresa: self::EMPRESA);
     }
 }
--- views/pages/painel/parceiro_externo/config/visualizar.php
+++ views/pages/painel/parceiro_externo/config/visualizar.php
@@ -1,18 +1,27 @@
 <?php
 
-use App\Classes\ParceiroLoja\Status;
 use App\Classes\ParceiroLoja\Categoria;
+use App\Classes\ParceiroLoja\Status;
+use PainelConfig\Visualizar;
 
-$Painel = new PainelConfig\Visualizar('parceiro_externo');
+$Painel = new Visualizar('parceiro_externo');
 
 $Painel->coluna(callback: function () use ($Painel) {
-    $Painel->bloco(titulo: 'Parceiro', callback: function () use ($Painel) {
+    $Painel->bloco('Parceiro', function () use ($Painel) {
         $Painel
             ->linha('titulo_interno', 'Título')
             ->linha('categoria_principal', 'Categoria')
-            ->dataHora('data_criacao', 'Criado em')
             ->linha('tipo_indicador', 'Tipo indicador')
+            ->dataHora('data_criacao', 'Criado em')
             ->linha('status', 'Status');
+    });
+
+    $Painel->bloco('Contato', function () use ($Painel) {
+        $Painel
+            ->linha('contato->nome', 'Nome')
+            ->linha('contato->cpf', 'CPF')
+            ->linha('contato->email', 'E-mail')
+            ->linha('contato->telefone', 'Telefone');
     });
 });
 
