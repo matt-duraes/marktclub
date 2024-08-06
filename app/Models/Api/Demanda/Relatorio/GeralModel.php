eb2d51daba87beadda323bc5c9b5335b9983a715
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-08-06 11:33:46 -0300

    fix: corrigindo relatorio da demanda

--- app/Models/Api/Demanda/Relatorio/GeralModel.php
+++ app/Models/Api/Demanda/Relatorio/GeralModel.php
@@ -70,10 +70,7 @@ abstract class GeralModel extends ORM
     private function salvar()
     {
         foreach ($this->relatorio as $dado) {
-            if(
-                (array_key_exists('quantidade_demanda', $dado) && empty($dado['quantidade_demanda'])) ||
-                (array_key_exists('quantidade_tarefa', $dado) && empty($dado['quantidade_tarefa']))
-            ) {
+            if(array_key_exists('quantidade_demanda', $dado) && empty($dado['quantidade_demanda'])) {
                 continue;
             }
             $this->dado($dado)->insert();
