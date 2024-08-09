b61587f559de34ee92cffc8b16eaa48f3a34ddc6
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-08-09 17:10:43 -0300

    feat: criando tipo loja pagina

--- views/pages/painel/parceiro_loja/config/salvar.php
+++ views/pages/painel/parceiro_loja/config/salvar.php
@@ -9,12 +9,12 @@ return [
         'titulo_interno', 'tipo_loja', 'equipe', 'responsavel_nome', 'responsavel_cpf', 'responsavel_email',
         'responsavel_telefone', 'responsavel_cargo', 'destaque', 'endereco_estado', 'pontuacao',
         'imagem_logo', 'imagem_capa_desktop', 'imagem_capa_mobile', 'titulo', 'tipo_estabelecimento', 'origem_lead',
-        'url', 'delivery', 'data_contrato_inicio', 'data_contrato_vencimento', 'precisa_aditivo', 'desconto',
+        'url', 'app', 'delivery', 'data_contrato_inicio', 'data_contrato_vencimento', 'precisa_aditivo', 'desconto',
         'email_contato', 'tipo_procedimento', 'limite_voucher', 'prazo_voucher', 'prazo_voucher_fixo',
         'contato_whatsapp', 'link_site', 'link_alias', 'link_bloqueado', 'texto_descricao', 'texto_desconto',
         'texto_procedimento', 'texto_voucher', 'texto_restricao', 'texto_outro', 'categoria_principal',
         'categoria_lista', 'subcategoria_tag', 'comissao_minima', 'comissao_maxima',
         'subcategoria_lista', 'empresa', 'confirmar_status', 'confirmar_titulo', 'confirmar_texto',
-        'arquivo_painel', 'arquivo_clube', 'cupom_desconto', 'status'
+        'arquivo_painel', 'arquivo_clube', 'cupom_desconto', 'pagina_app'
     ]
 ];
