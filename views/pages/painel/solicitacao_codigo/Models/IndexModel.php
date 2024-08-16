245b92fe747e53e205c0a57a3c25c771b4447edd
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-08-16 14:36:52 -0300

    improvement: removendo filtro padrao

--- views/pages/painel/solicitacao_codigo/Models/IndexModel.php
+++ views/pages/painel/solicitacao_codigo/Models/IndexModel.php
@@ -2,14 +2,13 @@
 
 namespace Painel\SolicitacaoCodigo\Models;
 
-use App\Classes\SolicitacaoCodigo\Status;
 use System\Interface\PainelIndexFiltroInterface;
 
 final class IndexModel implements PainelIndexFiltroInterface
 {
     public function filtro(array $filtro, string $pesquisa, string $ordem, int $pagina): array
     {
-        $filtro['status'] = Status::ABERTO;
+        /*$filtro['status'] = Status::ABERTO;*/
         return $filtro;
     }
 }
