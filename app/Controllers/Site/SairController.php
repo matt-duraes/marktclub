4464eecd2a4b90e7c3d989890e8bb8f4b1145510
Mateus Durães <mateusduraessilva@gmail.com>
2024-08-26 15:52:15 -0300

    fix: ajustes no botao de sair personalizado

--- app/Controllers/Site/SairController.php
+++ app/Controllers/Site/SairController.php
@@ -9,24 +9,13 @@ final class SairController extends Controller
 {
     public function index()
     {
-        return $this->destruirSessao(LINK . '/login');
-    }
-
-    public function sairPersonalizado()
-    {
-        return $this->destruirSessao(LINK_BOTAO_SAIR);
-    }
-
-    /**
-     * Destruir sessão e redirecionar para página de login ou link personalizado
-     *
-     * @param  string   $link_sair
-     * @return Response
-     */
-    private function destruirSessao(string $link_sair): Response
-    {
         sessaoDestruir();
         cookieDeletar('CLT');
-        return new Response(url: $link_sair);
+
+        if(LINK_BOTAO_SAIR) {
+            return new Response(url: LINK_BOTAO_SAIR);
+        }
+
+        return new Response(url: LINK . '/login');
     }
 }
