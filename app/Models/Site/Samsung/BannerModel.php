f836bf40b545afb38f3c8553836f8b19ca60605a
Mateus Durães <mateusduraessilva@gmail.com>
2024-11-07 16:56:38 -0300

    fix: adicionando link do samsungFixo

--- app/Models/Site/Samsung/BannerModel.php
+++ app/Models/Site/Samsung/BannerModel.php
@@ -87,15 +87,13 @@ final class BannerModel extends ClubeApiHelper
                 (object) [
                     'imagem' => LINK . '/images/site/banner_samsung_fixo_desktop.png',
                     'target' => '',
-                    'link'   => ''
-                ]
+                    'link'   => 'https://samsung.com.br/services/cartao-samsung/'                ]
             ],
             'mobile' => [
                 (object) [
                     'imagem' => LINK . '/images/site/banner_samsung_fixo_mobile.png',
                     'target' => '',
-                    'link'   => ''
-                ]
+                    'link'   => 'https://samsung.com.br/services/cartao-samsung/'                ]
             ]
         ];
     }
