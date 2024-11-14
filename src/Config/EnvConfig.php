0d5b1de5f71f0226e3360e562a39afee181c7074
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-11-14 12:49:51 -0300

    feat: criando sistema de env.chave em local

--- src/Config/EnvConfig.php
+++ src/Config/EnvConfig.php
@@ -57,15 +57,22 @@ final class EnvConfig
      */
     private function pegarEnvProducao(): void
     {
-        $tmpFwArquivo = __DIR__ . '/../../env/.env';
-        $arquivo = file_exists($tmpFwArquivo) ? $tmpFwArquivo : __DIR__ . '/../../.env';
-
+        $arquivo = __DIR__ . '/../../env/.env';
         if (!file_exists($arquivo)) {
             throw new Exception(message: 'Você precisa criar um arquivo de configuração .env na raiz do seu projeto.');
         }
 
         $env = $this->montarEnv($arquivo);
-
+        $arquivoChave = __DIR__ . '/../../env/.env.chave';
+        if(file_exists($arquivoChave) && eLocalhost()) {
+            $envChave = $this->montarEnv($arquivoChave);
+            foreach($envChave as $ind => $val) {
+                if(array_key_exists($ind, $env)) {
+                    continue;
+                }
+                $env[$ind] = $val;
+            }
+        }
         $this->envUso = $env;
         $this->envProducao = $env;
         $this->setTipoArquivo($env['APP_TIPO'] ?? 'producao');
@@ -122,12 +129,7 @@ final class EnvConfig
      */
     private function pegarEnvLocal(): void
     {
-        $tmpFwLista = [];
-        if(is_dir($this->root . '/env')) {
-            $tmpFwLista = array_diff(scandir($this->root . '/env'), ['.', '..']);
-        }
-        $listaEnv = $tmpFwLista ? $tmpFwLista : array_diff(scandir($this->root), ['.', '..']);
-        $arquivo = array_filter($listaEnv, function ($valor) {
+        $arquivo = array_filter(array_diff(scandir($this->root . '/env'), ['.', '..']), function ($valor) {
             if (preg_match('/^\.env\./', $valor)) {
                 return $valor;
             }
@@ -137,7 +139,7 @@ final class EnvConfig
         }
 
         foreach ($arquivo as $arquivo) {
-            $env = $this->montarEnv($this->root . '/' . $arquivo);
+            $env = $this->montarEnv($this->root . '/env/' . $arquivo);
             $url = $env['APP_URL'] ?? '';
             if (
                 !empty($url) &&
