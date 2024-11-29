29fef56fa6d1e72bac8aba702fd092c788b057d4
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-11-29 11:57:55 -0300

    improvement: adicionando novo campo na tabela

--- database/site_lotacao/base.php
+++ database/site_lotacao/base.php
@@ -8,6 +8,7 @@ return (new DataBase())
     ->int('id_admin_empresa')->relacionado(TABELA_COMERCIAL_EMPRESA, 'id')
     ->slug('slug', 'titulo')->tamanho(256)
     ->varchar('titulo')->tamanho(256)
+    ->int('principal')->tamanho(1)->null()
     ->status()
     ->dataCriacao()
     ->dataAtualizacao();
