e1dd00f5d6a73de58d59c85a2e8b38f95db7415f
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2024-12-06 11:37:17 -0300

    fix: isso corrige as demandas d painel

--- app/Classes/ApiApp/Scope.php
+++ app/Classes/ApiApp/Scope.php
@@ -326,6 +326,7 @@ final class Scope
 
         'comercial_empresa:atualizar', 'comercial_empresa:buscar', 'comercial_empresa:listar',
         'comercial_empresa:salvar', 'comercial_popup:atualizar', 'comercial_popup:buscar',
+        'comercial_empresa:perfil',
 
         'comercial_popup:deletar', 'comercial_popup:expirado', 'comercial_popup:listar',
         'comercial_popup:ordenar', 'comercial_popup:salvar', 'comercial_restricao:listar',
@@ -349,7 +350,7 @@ final class Scope
         'demanda_sprint:buscar', 'demanda_sprint:demanda', 'demanda_sprint:listar',
         'demanda_sprint:salvar', 'demanda_trabalho:atualizar', 'demanda_trabalho:salvar',
         'demanda_tarefa:atualizar', 'demanda_tarefa:deletar', 'demanda_tarefa:like',
-        'demanda_tarefa:salvar', 'demanda_tarefa:buscar',
+        'demanda_tarefa:salvar', 'demanda_tarefa:buscar', 'demanda_tarefa:listar',
 
         'enquete_satisfacao:listar', 'enquete_satisfacao:buscar', 'enquete_satisfacao:atualizar',
 
