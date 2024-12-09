5e2659ea673524f2608be224742708b8eb1408e3
Andre Rodrigues <andrerodrigues@andrerodrigues.com>
2024-12-09 20:07:55 -0300

    fix: corrigindo decode login do painel

--- database/replace.php
+++ database/replace.php
@@ -11,15 +11,15 @@ return [
         'cod' => 'uuid'
     ],
     'tag_novo' => [
-        'cod' => 'uuid',
+        'cod'  => 'uuid',
         'menu' => 'clube'
     ],
     'solicitacao_cheque_bonus' => [
-        'cod' => 'uuid',
+        'cod'                  => 'uuid',
         'dependente_documento' => 'dependente_cpf'
     ],
     'solicitacao_declaracao' => [
-        'cod' => 'uuid',
+        'cod'     => 'uuid',
         'empresa' => 'id_admin_empresa',
         'usuario' => 'id_usuario_cliente',
         'vinculo' => 'id_parceiro_loja'
@@ -28,15 +28,15 @@ return [
         'cod' => 'uuid'
     ],
     'usuario_novo' => [
-        'cod' => 'uuid',
-        'empresa' => 'id_admin_empresa',
-        'titular' => 'id_usuario_cliente',
-        'documento' => 'cpf',
-        'sexo' => 'genero',
+        'cod'         => 'uuid',
+        'empresa'     => 'id_admin_empresa',
+        'titular'     => 'id_usuario_cliente',
+        'documento'   => 'cpf',
+        'sexo'        => 'genero',
         'aniversario' => 'data_nascimento',
-        'cidade' => 'endereco_cidade',
-        'uf' => 'endereco_estado',
-        'hash' => 'hash_valor'
+        'cidade'      => 'endereco_cidade',
+        'uf'          => 'endereco_estado',
+        'hash'        => 'hash_valor'
     ],
     'usuario_indicacao' => [
         'cod' => 'uuid'
