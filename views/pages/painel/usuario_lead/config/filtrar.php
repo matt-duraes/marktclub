87d0e2016d1ed55365a91aca4c17d260d08aa2d0
Mateus Durães <mateusduraessilva@gmail.com>
2025-02-21 16:27:45 -0300

    feat: adicionando novos filtros na pagina de usuario lead

--- views/pages/painel/usuario_lead/config/filtrar.php
+++ views/pages/painel/usuario_lead/config/filtrar.php
@@ -1,16 +1,15 @@
 <?php
 
-use App\Classes\UsuarioCliente\Origem;
 use App\Classes\UsuarioLead\Status;
+use App\Classes\UsuarioCliente\Origem;
 
 $Painel = new PainelConfig\Filtrar('usuario_lead');
 
 $Painel
     ->input(name: 'nome', titulo: 'Nome', label: 'Nome', placeholder: 'Digite um nome')
     ->email(name: 'email', titulo: 'E-mail', label: 'E-mail', placeholder: 'Digite um e-mail')
-    ->cpf(name: 'cpf', titulo: 'CPF', label: 'CPF', placeholder: 'Digite um CPF')
-    ->numero(name: 'matricula', titulo: 'Matrícula', label: 'Matrícula', placeholder: 'Digite uma matrícula')
     ->numero(name: 'siape', titulo: 'SIAPE', label: 'SIAPE', placeholder: 'Digite um SIAPE')
+    ->cpf(name: 'cpf', titulo: 'CPF', label: 'CPF', placeholder: 'Digite um CPF')
     ->bloco(function () use ($Painel) {
         $Painel
             ->select(name: 'origem', lista: (new Origem())->select(), titulo: 'Origem', label: 'Origem')
