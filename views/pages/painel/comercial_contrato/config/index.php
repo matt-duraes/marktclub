6706d025e8e73e1b651a1c905e97de391269ed5e
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2025-03-26 16:55:00 -0300

    improvement: isso melhora a nomeclatura da coluna em contratos

--- views/pages/painel/comercial_contrato/config/index.php
+++ views/pages/painel/comercial_contrato/config/index.php
@@ -9,7 +9,7 @@ $Painel = new Index('comercial_contrato', new Ordem());
 $Painel
     ->campo('titulo', 'Titulo', Index::TIPO_GRANDE)
     ->campo('empresa_nome', 'Empresa Indicação (Usuário Indicou)', Index::TIPO_GRANDE)
-    ->campo('equipe_nome', 'Responsável pela Prospecção', Index::TIPO_GRANDE)
+    ->campo('equipe_nome', 'Gestor do Contrato', Index::TIPO_GRANDE)
     ->dataCriacao()
     ->status('status', 'Status', new Status());
 
