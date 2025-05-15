dcaddec98797978a4fc6f3f8bf680c3ee0068838
André Rodrigues <andre@youhuul.com.br>
2025-05-15 17:08:10 -0300

    feat: corrigindo buscar plano no painel

--- app/Models/Api/Saude/Contratacao/ContratacaoEntity.php
+++ app/Models/Api/Saude/Contratacao/ContratacaoEntity.php
@@ -2,92 +2,138 @@
 
 namespace App\Models\Api\Saude\Contratacao;
 
-use ORM\Entity;
-use Modules\Cpf;
-use Erro\Excecao;
-use Modules\Data;
-use Modules\Nome;
-use Modules\Email;
-use Modules\Genero;
-use Modules\Telefone;
-use Helpers\OrmHelper;
-use Modules\EnderecoCep;
-use Modules\EstadoCivil;
-use Modules\EnderecoEstado;
-use App\Classes\Saude\Status;
-use App\Classes\Saude\Operadora;
 use App\Classes\Saude\Acomodacao;
-use App\Models\Api\Trait\ValidarEmpresaTrait;
-use App\Classes\Saude\Operadoras\Amil\Regioes;
-use App\Models\Api\Saude\Simulacao\SimulacaoEntity;
+use App\Classes\Saude\Operadora;
 use App\Classes\Saude\Operadoras\Amil\Planos as PlanoAmil;
-use App\Classes\SaudeSimulacao\Status as SaudeSimulacaoStatus;
+use App\Classes\Saude\Operadoras\Amil\Regioes;
 use App\Classes\Saude\Operadoras\CNUFlorianopolis\Planos as PlanoCNU;
+use App\Classes\Saude\Status;
+use App\Classes\SaudeSimulacao\Status as SaudeSimulacaoStatus;
+use App\Models\Api\Saude\Simulacao\SimulacaoEntity;
+use App\Models\Api\Trait\ValidarEmpresaTrait;
+use Erro\Excecao;
+use Helpers\OrmHelper;
+use Modules\Cpf;
+use Modules\Data;
+use Modules\Email;
+use Modules\EnderecoCep;
+use Modules\EnderecoEstado;
+use Modules\EstadoCivil;
+use Modules\Genero;
+use Modules\Nome;
+use Modules\Telefone;
+use ORM\Entity;
 
 class ContratacaoEntity extends Entity
 {
     use ValidarEmpresaTrait;
 
-    public array $usuario;
-    public array $empresa;
-    public array $simulacao;
-    public Cpf $documento_cpf;
-    public string $documento_rg;
-    public string $orgao_expedidor;
-    public Nome $nome;
-    public Data $data_nascimento;
-    public EstadoCivil $estado_civil;
-    public string $naturalidade;
-    public Genero $genero;
-    public string $peso;
-    public string $altura;
-    public Nome $nome_mae;
-    public Cpf $responsavel_cpf;
-    public string $responsavel_rg;
-    public Nome $responsavel_nome;
-    public Email $email_pessoal;
-    public Telefone $telefone_celular;
-    public Telefone $telefone_residencial;
-    public Telefone $telefone_comercial;
-    public string $telefone_comercial_ramal;
-    public string $endereco_logradouro;
-    public EnderecoCep $endereco_cep;
+    public array          $usuario;
+    public array          $empresa;
+    public array          $simulacao;
+    public Cpf            $documento_cpf;
+    public string         $documento_rg;
+    public string         $orgao_expedidor;
+    public Nome           $nome;
+    public Data           $data_nascimento;
+    public EstadoCivil    $estado_civil;
+    public string         $naturalidade;
+    public Genero         $genero;
+    public string         $peso;
+    public string         $altura;
+    public Nome           $nome_mae;
+    public Cpf            $responsavel_cpf;
+    public string         $responsavel_rg;
+    public Nome           $responsavel_nome;
+    public Email          $email_pessoal;
+    public Telefone       $telefone_celular;
+    public Telefone       $telefone_residencial;
+    public Telefone       $telefone_comercial;
+    public string         $telefone_comercial_ramal;
+    public string         $endereco_logradouro;
+    public EnderecoCep    $endereco_cep;
     public EnderecoEstado $endereco_estado;
-    public string $endereco_cidade;
-    public string $endereco_bairro;
-    public int $endereco_numero;
-    public string $endereco_complemento;
-    public Status $status;
-    public string $responsavel_orgao_expedidor;
-    protected int $id_saude_simulacao;
-    private int $idEmpresa;
-    private ?int $idUsuario = null;
-    protected int $idSimulacao;
-    protected int $id_usuario_cliente;
-    protected int $id_admin_empresa;
-    protected string $ormTabela = TABELA_SAUDE_CONTRATACAO;
-    protected array $ormInsert = [
+    public string         $endereco_cidade;
+    public string         $endereco_bairro;
+    public int            $endereco_numero;
+    public string         $endereco_complemento;
+    public Status         $status;
+    public string         $responsavel_orgao_expedidor;
+    protected int         $id_saude_simulacao;
+    protected int         $idSimulacao;
+    protected int         $id_usuario_cliente;
+    protected int         $id_admin_empresa;
+    protected string      $ormTabela        = TABELA_SAUDE_CONTRATACAO;
+    protected array       $ormInsert        = [
         'id_admin_empresa'   => '->idEmpresa',
         'id_usuario_cliente' => '->idUsuario',
-        'status'             => 1
+        'status'             => 1,
     ];
-    protected array $ormBuscar = [
-        'id_saude_simulacao', 'id_usuario_cliente', 'id_admin_empresa', 'documento_cpf', 'documento_rg',
-        'orgao_expedidor', 'nome', 'data_nascimento', 'estado_civil', 'naturalidade', 'genero', 'peso', 'altura',
-        'nome_mae', 'responsavel_cpf', 'responsavel_rg', 'responsavel_nome', 'responsavel_orgao_expedidor',
-        'email_pessoal', 'telefone_celular', 'telefone_residencial', 'telefone_comercial',
-        'telefone_comercial_ramal', 'endereco_logradouro', 'endereco_cep', 'endereco_estado',
-        'endereco_cidade', 'endereco_bairro', 'endereco_numero', 'endereco_complemento', 'status',
+    protected array       $ormBuscar        = [
+        'id_saude_simulacao',
+        'id_usuario_cliente',
+        'id_admin_empresa',
+        'documento_cpf',
+        'documento_rg',
+        'orgao_expedidor',
+        'nome',
+        'data_nascimento',
+        'estado_civil',
+        'naturalidade',
+        'genero',
+        'peso',
+        'altura',
+        'nome_mae',
+        'responsavel_cpf',
+        'responsavel_rg',
+        'responsavel_nome',
+        'responsavel_orgao_expedidor',
+        'email_pessoal',
+        'telefone_celular',
+        'telefone_residencial',
+        'telefone_comercial',
+        'telefone_comercial_ramal',
+        'endereco_logradouro',
+        'endereco_cep',
+        'endereco_estado',
+        'endereco_cidade',
+        'endereco_bairro',
+        'endereco_numero',
+        'endereco_complemento',
+        'status',
     ];
-    protected array $ormSalvar = [
-        'id_saude_simulacao', 'documento_cpf', 'documento_rg', 'orgao_expedidor', 'nome',
-        'data_nascimento', 'estado_civil', 'naturalidade', 'genero', 'peso', 'altura',
-        'nome_mae', 'responsavel_cpf', 'responsavel_rg', 'responsavel_nome', 'responsavel_orgao_expedidor',
-        'email_pessoal', 'telefone_celular', 'telefone_residencial', 'telefone_comercial',
-        'telefone_comercial_ramal', 'endereco_logradouro', 'endereco_cep', 'endereco_estado',
-        'endereco_cidade', 'endereco_bairro', 'endereco_numero', 'endereco_complemento', 'status'
+    protected array       $ormSalvar        = [
+        'id_saude_simulacao',
+        'documento_cpf',
+        'documento_rg',
+        'orgao_expedidor',
+        'nome',
+        'data_nascimento',
+        'estado_civil',
+        'naturalidade',
+        'genero',
+        'peso',
+        'altura',
+        'nome_mae',
+        'responsavel_cpf',
+        'responsavel_rg',
+        'responsavel_nome',
+        'responsavel_orgao_expedidor',
+        'email_pessoal',
+        'telefone_celular',
+        'telefone_residencial',
+        'telefone_comercial',
+        'telefone_comercial_ramal',
+        'endereco_logradouro',
+        'endereco_cep',
+        'endereco_estado',
+        'endereco_cidade',
+        'endereco_bairro',
+        'endereco_numero',
+        'endereco_complemento',
+        'status',
     ];
-    protected string $ormValidarInsert = '
+    protected string      $ormValidarInsert = '
         documento_cpf|CPF|obrigatorio|vazio|valido
         documento_rg|RG|obrigatorio|vazio
         orgao_expedidor|Orgão Expedidor|obrigatorio|vazio
@@ -115,6 +161,8 @@ class ContratacaoEntity extends Entity
         endereco_numero|Número|obrigatorio|vazio
         endereco_complemento|Complemento
     ';
+    private int           $idEmpresa;
+    private ?int          $idUsuario        = null;
 
     /**
      * @param SimulacaoEntity $Simulacao
@@ -163,7 +211,7 @@ class ContratacaoEntity extends Entity
         $this->usuario = [
             'id'    => $usuario->uuid,
             'nome'  => $usuario->nome,
-            'email' => $usuario->email_pessoal
+            'email' => $usuario->email_pessoal,
         ];
     }
 
@@ -177,7 +225,7 @@ class ContratacaoEntity extends Entity
 
         $this->empresa = [
             'id'   => $empresa->uuid,
-            'nome' => $empresa->nome_fantasia
+            'nome' => $empresa->nome_fantasia,
         ];
     }
 
@@ -185,8 +233,20 @@ class ContratacaoEntity extends Entity
     {
         $simulacao = (new OrmHelper(TABELA_SAUDE_SIMULACAO))->pegarUltimoRegistro(
             ['id', $this->id_saude_simulacao],
-            ['uuid', 'titular', 'quantidade_dependente', 'operadora', 'acomodacao', 'plano',
-                'regiao', 'valor_titular', 'lista_dependente', 'valor_total', 'data_criacao', 'status'],
+            [
+                'uuid',
+                'titular',
+                'quantidade_dependente',
+                'operadora',
+                'acomodacao',
+                'plano',
+                'regiao',
+                'valor_titular',
+                'lista_dependente',
+                'valor_total',
+                'data_criacao',
+                'status',
+            ],
             'object'
         );
 
@@ -211,8 +271,8 @@ class ContratacaoEntity extends Entity
 
         foreach ($listaDependente as $dependente) {
             $lista[] = [
-                'data_nascimento'  => $dependente->data_nascimento,
-                'valor'            => $dependente->valor
+                'data_nascimento' => $dependente->data_nascimento,
+                'valor'           => $dependente->valor,
             ];
         }
         return $lista;
@@ -228,35 +288,47 @@ class ContratacaoEntity extends Entity
                     'operadora'  => $operadora,
                     'plano'      => (new PlanoAmil($simulacao->plano))->indice(),
                     'regiao'     => (new Regioes($simulacao->regiao))->indice(),
-                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice()
+                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice(),
                 ];
             case Operadora::CNU_FLORIANOPIS:
                 return [
                     'operadora'  => $operadora,
                     'plano'      => (new PlanoCNU($simulacao->plano))->indice(),
                     'regiao'     => $simulacao->regiao,
-                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice()
+                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice(),
                 ];
             case Operadora::UNIMED:
                 return [
                     'operadora'  => $operadora,
                     'plano'      => $simulacao->plano,
                     'regiao'     => $simulacao->regiao,
-                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice()
+                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice(),
                 ];
             case Operadora::UNIMED_SEGURO:
                 return [
                     'operadora'  => $operadora,
                     'plano'      => $simulacao->plano,
                     'regiao'     => $simulacao->regiao,
-                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice()
+                    'acomodacao' => (new Acomodacao($simulacao->acomodacao))->indice(),
+                ];
+            case Operadora::UNIMED_NATAL:
+                return [
+                    'operadora' => $operadora,
+                    'plano'     => $simulacao->plano,
+                    'regiao'    => $simulacao->regiao,
+                ];
+            case Operadora::UNIMED_JUNDIAI:
+                return [
+                    'operadora' => $operadora,
+                    'plano'     => $simulacao->plano,
+                    'regiao'    => $simulacao->regiao,
                 ];
             default:
                 return [
                     'operadora'  => '',
                     'plano'      => '',
                     'regiao'     => '',
-                    'acomodacao' => ''
+                    'acomodacao' => '',
                 ];
         }
     }
