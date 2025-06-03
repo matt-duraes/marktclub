806649e1eae54090fc3fab3e43bc961090267e80
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2025-06-03 12:01:46 -0300

    improvement: isso adiciona download no enquete mercado

--- routes/ApiRoute.php
+++ routes/ApiRoute.php
@@ -2615,6 +2615,14 @@ Route
             ::nome('respondeu')
             ::middleware(TokenMiddleware::class, 'scope', ['enquete_mercado:buscar'])
             ::post('/pesquisa-resposta');
+
+        Route
+            ::nome('download')
+            ::middleware(TokenMiddleware::class, 'scope', ['enquete_mercado:download'])
+            ::request([
+                'campo', 'usuario'
+            ])
+            ::post('/enquete-mercado/download');
     });
 
 Route
