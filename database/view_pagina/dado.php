fa8649efa82b0f2d97b2d40bdde2d3d3f7494ee5
André Rodrigues <andre@youhuul.com.br>
2025-06-11 16:00:42 -0300

    feat: addicionando unimed vitoria e natal na base

--- database/view_pagina/dado.php
+++ database/view_pagina/dado.php
@@ -2,5 +2,6 @@
 
 return array(
     array('id' => '1','uuid' => '5e7239c0-2922-4a74-9af1-69003daf8579','titulo' => 'Unimed Vitória','url' => 'unimed-vitoria','status' => '1'),
-    array('id' => '2','uuid' => '18cc15b0-cc38-4342-a83d-ee387f099f1e','titulo' => 'Unimed Natal','url' => 'unimed-natal','status' => '1')
+    array('id' => '2','uuid' => '18cc15b0-cc38-4342-a83d-ee387f099f1e','titulo' => 'Unimed Natal','url' => 'unimed-natal','status' => '1'),
+    array('id' => '3','uuid' => 'c5f40848-ebc5-4e5f-8685-48bbb7467e54','titulo' => 'Unimed Jundiaí','url' => 'unimed-jundiai','status' => '1')
 );
