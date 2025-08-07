6389e9d18a017d7f79606453c5b7e18e7667e308
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2025-08-07 15:17:47 -0300

    Revert "fix: isso corrige busca para o site associacao"
    This reverts commit 4a25077e82537159e9c7a201da4873c9ba60c559.

--- app/Controllers/Api/SiteConfigController.php
+++ app/Controllers/Api/SiteConfigController.php
@@ -2,16 +2,16 @@
 
 namespace App\Controllers\Api;
 
-use App\Models\Api\SiteConfig\ConfigEntity;
-use App\Models\Api\SiteConfig\ConfigModel;
-use Controller\Controller;
 use Http\Request;
 use Http\Response;
-use System\Interface\ControllerAtualizarInterface;
+use Controller\Controller;
+use App\Models\Api\SiteConfig\ConfigModel;
+use App\Models\Api\SiteConfig\ConfigEntity;
 use System\Interface\ControllerBuscarInterface;
-use System\Interface\ControllerDeletarInterface;
 use System\Interface\ControllerListarInterface;
 use System\Interface\ControllerSalvarInterface;
+use System\Interface\ControllerDeletarInterface;
+use System\Interface\ControllerAtualizarInterface;
 
 final class SiteConfigController extends Controller implements
     ControllerListarInterface,
@@ -35,10 +35,21 @@ final class SiteConfigController extends Controller implements
         if (validarUuid($id, false)) {
             $Config->uuid($id);
         } else {
-            $Config->buscar(['link_site', 'like', $id]);
+            $Config->buscar([
+                ['link_site', 'json', $id]
+            ]);
         }
 
         return $this->retornoPadrao($Config);
+    }
+
+    public function postSalvar(Request $request): Response
+    {
+        $Config = new ConfigEntity();
+        $Config->set(lista: $request->dado());
+        $Config->salvar();
+
+        return $this->retornoPadrao($Config, 201);
     }
 
     private function retornoPadrao(ConfigEntity $Config, int $status = 200)
@@ -60,15 +71,6 @@ final class SiteConfigController extends Controller implements
             ),
             status: $status
         );
-    }
-
-    public function postSalvar(Request $request): Response
-    {
-        $Config = new ConfigEntity();
-        $Config->set(lista: $request->dado());
-        $Config->salvar();
-
-        return $this->retornoPadrao($Config, 201);
     }
 
     public function putAtualizar(Request $request, string $id): Response
